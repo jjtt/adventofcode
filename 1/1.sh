b181b4970e313d20bcc3dea6c8a7d2014350ca18
jjtt <3908945+jjtt@users.noreply.github.com>
2020-12-02 19:32:27 +0200

    Support <() as argument

--- 1/1.sh
+++ 1/1.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+tmp=$(mktemp)
+cat <"$1" > "$tmp"
+
 year=${2:-2020}
 
-grep -f <(awk "{print \"^\" ($year - \$1) \"$\"}" $1) $1 | paste - - | awk '{print $1 * $2}'
+grep -f <(awk "{print \"^\" ($year - \$1) \"$\"}" $tmp) $tmp | paste - - | awk '{print $1 * $2}'
+
+rm "$tmp"
