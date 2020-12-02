fefb484f42ecd08d150ab09fc4bf5d9dffa2f83c
jjtt <3908945+jjtt@users.noreply.github.com>
2020-12-02 19:10:35 +0200

    Year as parameter with 2020 default

--- 1/1.sh
+++ 1/1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-grep -f <(awk '{print "^" (2020 - $1) "$"}' $1) $1 | paste - - | awk '{print $1 * $2}'
+year=${2:-2020}
+
+grep -f <(awk "{print \"^\" ($year - \$1) \"$\"}" $1) $1 | paste - - | awk '{print $1 * $2}'
