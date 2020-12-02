b5960e00095067458de91d8e28b39245f547a1f0
jjtt <3908945+jjtt@users.noreply.github.com>
2020-12-02 14:40:49 +0200

    No xargs

--- 1/1.sh
+++ 1/1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sort $1 | awk '{print 2020 - $1}' | xargs -n 1 -I{} grep '^{}$' $1 | paste - - | awk '{print $1 * $2}'
+grep -f <(sort $1 | awk '{print "^" (2020 - $1) "$"}') $1 | paste - - | awk '{print $1 * $2}'
