fb760a355e002a8236009beb05a27b41225c9a70
jjtt <3908945+jjtt@users.noreply.github.com>
2020-12-03 11:59:53 +0200

    Generalise 1

---
+++ 3/2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+
+def is_tree(lines, r, c):
+    c = c % len(lines[r])
+    return lines[r][c] == '#'
+
+
+lines = [l.rstrip() for l in sys.stdin.readlines()]
+
+trees = 0
+
+r = 0
+c = 0
+down = 1
+right = 3
+
+while r < len(lines):
+    if is_tree(lines, r, c):
+        trees = trees + 1
+    c = c + right
+    r = r + down
+
+print(trees)
+
