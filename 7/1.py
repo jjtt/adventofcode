0620129ef6d057c35939adf30d3de20dccc8cc97
jjtt <3908945+jjtt@users.noreply.github.com>
2020-12-07 09:28:00 +0200

    A db, but what now?

--- 7/1.py
+++ 7/1.py
@@ -2,17 +2,34 @@
 
 import sys
 import re
+import pprint
 
 def normalize(s):
     s = s.replace('.', '')
     s = s.replace('bags', 'bag')
     return s
 
+def num(s):
+    i, n = s.split(' ', 1)
+    try:
+        return int(i), n
+    except ValueError:
+        return 0, n
+
 lines = [l.rstrip() for l in sys.stdin.readlines()]
+
+db = {}
 
 for l in lines:
     outer, inner = re.split(r" contain ", l)
     outer = normalize(outer)
     inner = normalize(inner)
-    inner = [s.strip() for s in inner.split(',')]
-    print(outer, inner)
+    inner = [num(s.strip()) for s in inner.split(',')]
+    for n,i in inner:
+        li = db.get(outer, [])
+        if not i == 'other bag':
+            li.append(i)
+        db[outer] = li
+
+pprint.pp(db)
+
