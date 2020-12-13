5e93f0d1213245e335df888ab74067c9c1851a55
jjtt <3908945+jjtt@users.noreply.github.com>
2020-12-13 11:44:00 +0200

    Day 13 part 2 sample bruted

--- 13/2.py
+++ 13/2.py
@@ -6,6 +6,15 @@ lines = [l.rstrip() for l in sys.stdin.readlines()]
 
 busses = lines[-1].split(',')
 
-e = [f"(x+{i})%{b}" for i,b in zip(range(len(busses)), busses) if b != 'x'] + ['0']
+high = max([int(b) for b in busses if b != 'x'])
 
-print('='.join(e))
+e = {int(b):i for i,b in zip(range(len(busses)), busses) if b != 'x'}
+
+i = 1
+while any([((i*high) - e[high] + e[b]) % b for b in e.keys()]):
+    if (i % 10000000) == 0:
+        print(i)
+    i = i + 1
+
+print(i)
+print((i*high) - e[high])
