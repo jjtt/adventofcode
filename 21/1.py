7579d90aae9de87b7e03ccaa1e2b5b98751375bc
jjtt <3908945+jjtt@users.noreply.github.com>
2020-12-21 18:03:15 +0200

    Day 21 part 1

--- 21/1.py
+++ 21/1.py
@@ -4,17 +4,6 @@ import sys
 import itertools
 import pprint
 
-def remove_fixed(ing_by_al, a, i):
-    otherals = set(ing_by_al.pop(a)) - set(i)
-    new_ing_by_al = {}
-    for a2,i2 in ing_by_al.items():
-        if a == a2:
-            continue
-        new_ing_by_al[a2] = list(set(i2) - set(i))
-    return new_ing_by_al
-            
-        
-
 lines = [l.rstrip() for l in sys.stdin.readlines()] 
 
 foods = []
@@ -23,19 +12,26 @@ for l in lines:
     i,a = l.split(' (contains ')
     foods.append((tuple(i.split(' ')), tuple(a[:-1].split(', '))))
 
-facts = []
 allergens = set()
-for i,a in foods:
-    for al in a:
-        allergens.add(al)
-        for ing in i:
-            facts.append(f"{al}({ing}).")
-facts.sort()
+for _,alist in foods:
+    for a in alist:
+        allergens.add(a)
 
-for f in facts:
-    print(f)
+ingredients = set()
+for ilist,_ in foods:
+    for i in ilist:
+        ingredients.add(i)
 
-print("solve(", ','.join([a.upper() for a in allergens]), "):-", ', '.join([f"{a}({a.upper()})" for a in allergens] + [f"not({a.upper()}={b.upper()})" for a,b in itertools.combinations(allergens, 2)]), ".")
+ok = ingredients
+fixed = {}
+for a in allergens:
+    ing = ingredients
+    for i,alist in foods:
+        if a in alist:
+            ing = ing & set(i)
+    fixed[a] = ing
+    ok = ok - ing
 
-    
+print(sum([i in ok for ilist,_ in foods for i in ilist]))
+
 
