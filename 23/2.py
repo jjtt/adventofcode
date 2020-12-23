217299ef2b06cfd4305a815a42f2f2ccce5c177b
jjtt <3908945+jjtt@users.noreply.github.com>
2020-12-23 19:03:41 +0200

    numpy practice, but a complete waste of time

--- 23/2.py
+++ 23/2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import numpy as np
 
 def dest(picked, current, size):
     while True:
@@ -9,11 +10,6 @@ def dest(picked, current, size):
             current = size
         if current not in picked:
             return current
-
-def print_circle(indices):
-    print(indices)
-    d = {indices[v]: v for v in range(1,size+1)}
-    print([d[i] for i in sorted(d.keys())])
 
 def to_circle(indices):
     d = {indices[v]: v for v in range(1,size+1)}
@@ -32,44 +28,44 @@ extracups = range(max(clockwise)+1, max(clockwise)+cupcount+1)
 
 clockwise = clockwise + list(extracups)
 
-indices = list(range(size+1))
+indices = np.array(list(range(size+1)))
 for i in range(len(clockwise)):
     indices[clockwise[i]] = i
 
 head = clockwise[:4]
 
-print(clockwise)
-print(indices)
-print(to_circle(indices))
-print(head)
-
-
+nums = np.arange(indices.size)
+nums[0] = -42
+indices[0] = -42
 
 for i in range(rounds):
     if not i % 10000:
         print(f"round {i+1}")
-    print(f"round {i+1}")
-    print(to_circle(indices))
-    print(head)
+    #print(f"round {i+1}")
+    #print(to_circle(indices))
+    #print(head)
     d = dest(head[1:], head[0], size)
-    print(d)
+    #print(d)
     di = indices[d]
-    newhead = [0] * 4
-    for n in range(1, size+1):
-        if n == head[0]:
-            indices[n] = size-1
-        elif n not in head:
-            if indices[n] <= di:
-                indices[n] = (indices[n] - 4) % size
-            else:
-                indices[n] = (indices[n] - 1) % size
-        else:
-            indices[n] = (indices[n] + (di-4)) % size
-        
-        if indices[n] < 4:
-            newhead[indices[n]] = n
 
-    head = newhead
+
+    indices = np.select(
+        [
+            nums == -42,
+            nums == head[0],
+            np.logical_and(np.isin(nums,head,invert=True),indices<=di),
+            np.logical_and(np.isin(nums,head,invert=True),indices>di),
+            True,
+        ],
+        [
+            nums,
+            np.full_like(indices, size-1),
+            (indices - 4) % size,
+            (indices - 1) % size,
+            (indices + di-4) % size,
+        ]
+    )
+    head = np.concatenate((nums[indices==0], nums[np.logical_and(indices<4, indices>0)]))
     
 clockwise = to_circle(indices)
 one = clockwise.index(1)
