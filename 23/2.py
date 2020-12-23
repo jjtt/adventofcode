7d93ba5b692506b590aa66b0e36f98ef34a23a93
jjtt <3908945+jjtt@users.noreply.github.com>
2020-12-23 14:41:15 +0200

    Index manipulations

--- 23/2.py
+++ 23/2.py
@@ -10,6 +10,15 @@ def dest(picked, current, size):
         if current not in picked:
             return current
 
+def print_circle(indices):
+    print(indices)
+    d = {indices[v]: v for v in range(1,size+1)}
+    print([d[i] for i in sorted(d.keys())])
+
+def to_circle(indices):
+    d = {indices[v]: v for v in range(1,size+1)}
+    return [d[i] for i in sorted(d.keys())]
+
 rounds = int(sys.argv[1])
 
 lines = [l.rstrip() for l in sys.stdin.readlines()] 
@@ -23,27 +32,46 @@ extracups = range(max(clockwise)+1, max(clockwise)+cupcount+1)
 
 clockwise = clockwise + list(extracups)
 
+indices = list(range(size+1))
+for i in range(len(clockwise)):
+    indices[clockwise[i]] = i
+
+head = clockwise[:4]
+
+print(clockwise)
+print(indices)
+print(to_circle(indices))
+print(head)
+
+
 
 for i in range(rounds):
     if not i % 10000:
         print(f"round {i+1}")
-    current = clockwise.pop(0)
-    clockwise.append(current)
-    p = clockwise[:3]
-    del clockwise[:3]
-    d = dest(p, current, size)
+    print(f"round {i+1}")
+    print(to_circle(indices))
+    print(head)
+    d = dest(head[1:], head[0], size)
+    print(d)
+    di = indices[d]
+    newhead = [0] * 4
+    for n in range(1, size+1):
+        if n == head[0]:
+            indices[n] = size-1
+        elif n not in head:
+            if indices[n] <= di:
+                indices[n] = (indices[n] - 4) % size
+            else:
+                indices[n] = (indices[n] - 1) % size
+        else:
+            indices[n] = (indices[n] + (di-4)) % size
+        
+        if indices[n] < 4:
+            newhead[indices[n]] = n
 
-    ci = 0
-    while not clockwise[ci] == d:
-        ci += 1
-
-    nextstart = clockwise[:ci+1]
-    del clockwise[:ci+1]
-
-    clockwise[0:0] = p
-
-    clockwise[0:0] = nextstart
+    head = newhead
     
+clockwise = to_circle(indices)
 one = clockwise.index(1)
 out = []
 for i in range(one+1, one+3):
