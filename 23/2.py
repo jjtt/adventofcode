c4308fd7bffe9b7edfa12c67aabd2f1b83cac4fc
jjtt <3908945+jjtt@users.noreply.github.com>
2020-12-23 11:59:24 +0200

    Printing and thinking.
    Does not look like there's a shortcut available in the values
    remaining ordered. They get mixed up pretty fast especially
    with more rounds than items

--- 23/2.py
+++ 23/2.py
@@ -41,13 +41,17 @@ clockwise = deque(clockwise + list(extracups))
 for i in range(rounds):
     if not i % 10000:
         print(f"round {i+1}")
+    print(f"round {i+1}")
+    print(clockwise)
     current = clockwise.popleft()
     clockwise.append(current)
     p = []
     p.append(clockwise.popleft())
     p.append(clockwise.popleft())
     p.append(clockwise.popleft())
+    print(p)
     d = dest(p, current, size)
+    print(d)
 
     nextstart = deque([])
     c = clockwise.popleft()
@@ -68,3 +72,14 @@ out = []
 for i in range(one+1, one+3):
     out.append(clockwise[i%len(clockwise)])
 print(out)
+
+m = 0
+c = 0
+for i in range(1, size):
+    if clockwise[i-1]+1 == clockwise[i]:
+        c += 1
+    else:
+        m = max(m, c)
+        c = 0
+print(max(m,c))
+
