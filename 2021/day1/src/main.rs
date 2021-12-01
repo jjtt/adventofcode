ef0b7e44cb1f531007f6f1855ede4f4a6418f5e9
jjtt <3908945+jjtt@users.noreply.github.com>
2021-12-01 08:41:22 +0200

    plumbing

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,3 +1,30 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
     println!("Hello, world!");
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[cfg(test)]
+mod test {
+    use test_case::test_case;
+
+    use super::*;
+
+    #[test_case("sample1.txt" => is eq(7) ; "sample")]
+    fn part1(input: &str) -> i32 {
+        for line in read_lines(input).unwrap() {
+            println!("{}", line.unwrap())
+        }
+        42
+    }
 }
