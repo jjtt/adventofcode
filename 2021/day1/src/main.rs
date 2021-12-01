33b2d5bb77748109d7cd158fe59aa75315cdd028
jjtt <3908945+jjtt@users.noreply.github.com>
2021-12-01 08:57:04 +0200

    cargo fmt

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,7 +1,7 @@
+use std::collections::LinkedList;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::LinkedList;
 
 fn main() {
     println!("Hello, world!");
@@ -10,7 +10,9 @@ fn main() {
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
