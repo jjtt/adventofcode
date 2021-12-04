7fb5339c340181e99f3733cac1883cd4798d48b4
jjtt <3908945+jjtt@users.noreply.github.com>
2021-12-04 15:20:15 +0200

    Day 4 part 1

--- 2021/day4/src/main.rs
+++ 2021/day4/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(slice_group_by)]
 
-use std::collections::{HashMap, LinkedList};
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
 const SIZE: i32 = 5;
@@ -11,18 +11,18 @@ struct Tile {
     y: i32,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct Board {
     marked: Vec<Tile>,
     numbers: HashMap<i32, Tile>,
 }
 
 impl Board {
-    fn new(rows: &mut LinkedList<&str>) -> Board {
-        assert_eq!("", rows.pop_front().unwrap());
+    fn new(rows: &Vec<&str>) -> Board {
+        assert_eq!("", *rows.get(0).unwrap());
         let mut numbers = HashMap::new();
         for y in 0..SIZE {
-            let row = rows.pop_front().unwrap();
+            let row = rows.get(y as usize + 1).unwrap();
             for x in 0..SIZE {
                 let string = row.chars().skip(x as usize * 3).take(2).collect::<String>();
                 let number = string.trim().parse::<i32>().unwrap();
@@ -33,6 +33,13 @@ impl Board {
         Board {
             marked: vec![],
             numbers: numbers,
+        }
+    }
+
+    fn play(&mut self, num: i32) {
+        let tile = self.numbers.get(&num);
+        if tile.is_some() {
+            let _ = &self.marked.push(tile.unwrap().clone());
         }
     }
 
@@ -47,18 +54,21 @@ impl Board {
     }
 
     fn is_winner(&self) -> bool {
+        let mut by_rows = self.marked.clone();
+        by_rows.sort_by(|t1, t2| t1.y.cmp(&t2.y));
+        let mut by_cols = self.marked.clone();
+        by_cols.sort_by(|t1, t2| t1.x.cmp(&t2.x));
+
         self.marked.len() as i32 >= SIZE
-            && (self
-                .marked
+            && (by_cols
                 .group_by(|t1, t2| t1.x == t2.x)
                 .map(|col| col.len())
                 .max()
                 .unwrap() as i32
                 >= SIZE
-                || self
-                    .marked
+                || by_rows
                     .group_by(|t1, t2| t1.y == t2.y)
-                    .map(|col| col.len())
+                    .map(|row| row.len())
                     .max()
                     .unwrap() as i32
                     >= SIZE)
@@ -69,13 +79,21 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn play(boards: &Vec<Board>, numbers: Vec<i32>) -> (i32, &Board) {
-    (0, boards.get(0).unwrap())
+fn play(boards: &mut Vec<Board>, numbers: Vec<i32>) -> (i32, Board) {
+    for n in numbers.iter() {
+        for b in &mut *boards {
+            b.play(*n);
+
+            if b.is_winner() {
+                return (*n, b.clone());
+            }
+        }
+    }
+    panic!("No winners")
 }
 
 #[cfg(test)]
 mod test {
-    use std::collections::LinkedList;
     use test_case::test_case;
 
     use super::*;
@@ -141,31 +159,49 @@ mod test {
             numbers: HashMap::new(),
         }
         .is_winner());
+        assert!(Board {
+            marked: vec![
+                Tile { x: 4, y: 4 },
+                Tile { x: 4, y: 0 },
+                Tile { x: 3, y: 1 },
+                Tile { x: 4, y: 3 },
+                Tile { x: 1, y: 3 },
+                Tile { x: 2, y: 0 },
+                Tile { x: 2, y: 2 },
+                Tile { x: 0, y: 4 },
+                Tile { x: 1, y: 4 },
+                Tile { x: 0, y: 0 },
+                Tile { x: 1, y: 0 },
+                Tile { x: 3, y: 0 },
+            ],
+            numbers: HashMap::new(),
+        }
+        .is_winner());
     }
 
     #[test_case("sample1.txt" => is eq(4512) ; "sample")]
-    #[test_case("input.txt" => is eq(0) ; "input")]
+    #[test_case("input.txt" => is eq(33348) ; "input")]
     fn part1(input: &str) -> i32 {
         let input = read_to_string(input).unwrap();
 
-        let mut lines: LinkedList<&str> = input.trim().lines().collect();
+        let lines: Vec<&str> = input.trim().lines().collect();
 
         let numbers: Vec<i32> = lines
-            .pop_front()
+            .get(0)
             .unwrap()
             .split(",")
             .map(|n| n.parse::<i32>().unwrap())
             .collect();
 
         let mut boards: Vec<Board> = Vec::new();
-        while !lines.is_empty() {
-            boards.push(Board::new(&mut lines));
+        for b in lines[1..].chunks(SIZE as usize + 1) {
+            boards.push(Board::new(&b.to_vec()));
         }
 
         dbg!(&numbers);
         dbg!(&boards);
 
-        let (last_number, winner) = play(&boards, numbers);
+        let (last_number, winner) = play(&mut boards, numbers);
 
         last_number * winner.sum_unused()
     }
