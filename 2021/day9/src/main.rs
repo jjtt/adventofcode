a5d7e05812d34fba68e688ac1ae8c1cc9adcab2e
jjtt <3908945+jjtt@users.noreply.github.com>
2021-12-09 19:49:01 +0200

    Day9 part2

--- 2021/day9/src/main.rs
+++ 2021/day9/src/main.rs
@@ -43,7 +43,7 @@ impl Map {
     fn find_low_points(&self) -> Vec<Point> {
         let mut low_points = vec![];
 
-        for (point, value) in &self.map {
+        for point in self.map.keys() {
             if self.is_low_point(point) {
                 low_points.push((*point).clone());
             }
@@ -63,6 +63,23 @@ impl Map {
 
     fn value(&self, point: &Point) -> Option<&i32> {
         self.map.get(point)
+    }
+
+    fn basin_size(&self, point: &Point) -> i32 {
+        let mut basin = vec![];
+        self.basin(point, &mut basin);
+        basin.len() as i32
+    }
+
+    fn basin(&self, point: &Point, basin: &mut Vec<Point>) {
+        let value = self.value(point).unwrap_or(&i32::MAX);
+        if !basin.contains(&point) && *value < 9 {
+            basin.push(point.clone());
+            self.basin(&point.n(), basin);
+            self.basin(&point.s(), basin);
+            self.basin(&point.w(), basin);
+            self.basin(&point.e(), basin);
+        }
     }
 }
 
@@ -117,5 +134,24 @@ mod test {
         let low_points = map.find_low_points();
 
         low_points.iter().map(|p| map.value(p).unwrap() + 1).sum()
+    }
+
+    #[test_case("sample1.txt" => is eq(1134) ; "sample")]
+    #[test_case("input.txt" => is eq(916688) ; "input")]
+    fn part2(input: &str) -> i32 {
+        let input = read_to_string(input).unwrap();
+
+        let map = map_from_input(input);
+
+        let mut basin_sizes: Vec<i32> = map
+            .find_low_points()
+            .iter()
+            .map(|low| map.basin_size(low))
+            .collect();
+
+        basin_sizes.sort();
+        basin_sizes.reverse();
+
+        basin_sizes.get(0).unwrap() * basin_sizes.get(1).unwrap() * basin_sizes.get(2).unwrap()
     }
 }
