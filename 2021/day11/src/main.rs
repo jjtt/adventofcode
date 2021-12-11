0447b0eef6dc249b0dc3b9e3cf9d1cbe5b3f6f45
jjtt <3908945+jjtt@users.noreply.github.com>
2021-12-11 12:15:10 +0200

    Day11 part1

--- 2021/day11/src/main.rs
+++ 2021/day11/src/main.rs
@@ -4,6 +4,63 @@ fn main() {
     println!("Hello, world!");
 }
 
+fn step(octos: &mut Vec<Vec<u8>>) {
+    for y in 0..10 {
+        for x in 0..10 {
+            let o = octos.get_mut(y).unwrap().get_mut(x).unwrap();
+            *o += 1;
+        }
+    }
+}
+
+fn flash(octos: &mut Vec<Vec<u8>>) -> i32 {
+    let mut flashes = 0;
+    for y in 0..10 {
+        for x in 0..10 {
+            let o = octos.get_mut(y).unwrap().get_mut(x).unwrap();
+            if *o > 9 {
+                flashes += 1;
+                *o = 0;
+                energize_neighbours(octos, &x, &y);
+            }
+        }
+    }
+    let mut more = flashes > 0;
+    while more {
+        let more_flashes = flash(octos);
+        flashes += more_flashes;
+        more = more_flashes > 0;
+    }
+
+    flashes
+}
+
+fn energize_neighbours(octos: &mut Vec<Vec<u8>>, x: &usize, y: &usize) {
+    for ny in 0.max(*y as i32 - 1)..=9.min(*y as i32 + 1) {
+        for nx in 0.max(*x as i32 - 1)..=9.min(*x as i32 + 1) {
+            let o = octos
+                .get_mut(ny as usize)
+                .unwrap()
+                .get_mut(nx as usize)
+                .unwrap();
+            if *o > 0 {
+                *o += 1;
+            }
+        }
+    }
+}
+
+fn print(octos: &Vec<Vec<u8>>) {
+    dbg!(octos
+        .iter()
+        .map(|row| row
+            .iter()
+            .map(|o| o.to_string())
+            .collect::<Vec<String>>()
+            .join(""))
+        .collect::<Vec<String>>());
+}
+
 #[cfg(test)]
 mod test {
     use test_case::test_case;
@@ -11,12 +68,55 @@ mod test {
     use super::*;
 
     #[test_case("sample1.txt" => is eq(1656) ; "sample")]
-    #[test_case("input.txt" => is eq(0) ; "input")]
-    fn part1(input: &str) -> i128 {
-        read_to_string(input)
+    #[test_case("input.txt" => is eq(1649) ; "input")]
+    fn part1(input: &str) -> i32 {
+        let mut octos = read_to_string(input)
             .unwrap()
             .lines()
-            .map(|l| l.parse::<i128>().unwrap())
-            .sum()
+            .map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as u8).collect())
+            .collect();
+
+        let mut flashes = 0;
+
+        for _ in 0..100 {
+            step(&mut octos);
+
+            flashes += flash(&mut octos);
+        }
+
+        flashes
+    }
+
+    #[test]
+    fn first_steps() {
+        let mut octos = read_to_string("sample1.txt")
+            .unwrap()
+            .lines()
+            .map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as u8).collect())
+            .collect();
+
+        step(&mut octos);
+        assert_eq!(0, flash(&mut octos));
+        print(&octos);
+
+        step(&mut octos);
+        assert_eq!(35, flash(&mut octos));
+        print(&octos);
+
+        step(&mut octos);
+        assert_eq!(45, flash(&mut octos));
+        print(&octos);
+
+        step(&mut octos);
+        flash(&mut octos);
+        print(&octos);
+
+        step(&mut octos);
+        flash(&mut octos);
+        print(&octos);
+
+        step(&mut octos);
+        assert_eq!(1, flash(&mut octos));
+        print(&octos);
     }
 }
