64ba229e9b1127aafe6119e6f3bccaffe7fa964e
jjtt <3908945+jjtt@users.noreply.github.com>
2021-12-13 21:34:33 +0200

    Day13 part1

--- 2021/day13/src/main.rs
+++ 2021/day13/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 use std::fs::read_to_string;
 
@@ -6,18 +7,25 @@ fn main() {
 }
 
 fn game_from_input(input: &str) -> (HashSet<(i32, i32)>, Vec<(i32, i32)>) {
-    let dots = HashSet::new();
-    let folds = vec![];
+    let mut dots = HashSet::new();
+    let mut folds = vec![];
     let mut folding = false;
     for line in read_to_string(input).unwrap().lines() {
         if line == "" {
             folding = true;
-        }
-
-        if folding {
-            // folds
         } else {
-            // dots
+            if folding {
+                let (a, n) = line.split("=").collect_tuple().unwrap();
+                let num = n.parse().unwrap();
+                folds.push(if a.ends_with("x") { (0, num) } else { (1, num) });
+            } else {
+                dots.insert(
+                    line.split(",")
+                        .map(|n| n.parse().unwrap())
+                        .collect_tuple()
+                        .unwrap(),
+                );
+            }
         }
     }
 
@@ -31,10 +39,34 @@ mod test {
     use super::*;
 
     #[test_case("sample1.txt" => is eq(17) ; "sample1")]
-    #[test_case("input.txt" => is eq(0) ; "input")]
+    #[test_case("input.txt" => is eq(693) ; "input")]
     fn part1(input: &str) -> usize {
         let (dots, folds) = game_from_input(input);
 
-        0
+        let dots = fold(dots, *folds.first().unwrap());
+
+        dots.len()
+    }
+
+    fn fold(dots: HashSet<(i32, i32)>, (axis, num): (i32, i32)) -> HashSet<(i32, i32)> {
+        let mut folded = HashSet::new();
+
+        for (x, y) in dots {
+            if axis == 1 {
+                if y < num {
+                    folded.insert((x, y));
+                } else {
+                    folded.insert((x, num - (y - num)));
+                }
+            } else {
+                if x < num {
+                    folded.insert((x, y));
+                } else {
+                    folded.insert((num - (x - num), y));
+                }
+            }
+        }
+
+        folded
     }
 }
