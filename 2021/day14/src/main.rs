049950d2d50540aa3646f29c004db9a27514415c
jjtt <3908945+jjtt@users.noreply.github.com>
2021-12-14 19:30:57 +0200

    cleanup, but not much

--- 2021/day14/src/main.rs
+++ 2021/day14/src/main.rs
@@ -27,6 +27,67 @@ fn game_from_input(input: &str) -> (String, HashMap<String, String>) {
     )
 }
 
+fn count(polymer: String) -> HashMap<char, usize> {
+    let counts = polymer.chars().fold(HashMap::new(), |mut m, c| {
+        *m.entry(c).or_insert(0) += 1;
+        m
+    });
+    counts
+}
+
+fn count_most_least(counts: HashMap<char, usize>) -> (usize, usize) {
+    let counts = counts;
+    (
+        *counts.values().max().unwrap(),
+        *counts.values().min().unwrap(),
+    )
+}
+
+fn apply(template: String, rules: &HashMap<String, String>) -> String {
+    let mut polymer = template[..1].to_string();
+
+    for i in 0..template.len() - 1 {
+        let pair = &template[i..=i + 1];
+        let last = &pair[1..];
+        match rules.get(pair) {
+            Some(insert) => {
+                polymer += insert;
+                polymer += last
+            }
+            None => polymer += last,
+        }
+    }
+
+    polymer
+}
+
+fn merge(counts: &mut HashMap<char, usize>, more: &HashMap<char, usize>) {
+    for (c, n) in more {
+        *counts.entry(*c).or_insert(0) += *n;
+    }
+}
+
+cached_key! {
+    // name and type
+    NAME_OF_CACHE: UnboundCache<String, HashMap<char, usize>> = UnboundCache::new();
+    // construct key, cache is a shared one, so include rules in key
+    Key = { format!("{}{}{}", pair, rounds, rules.len())};
+    fn apply_rec(rules: &HashMap<String, String>, pair: &str, rounds: i32) -> HashMap<char, usize> = {
+        let mut counts = HashMap::new();
+        if rounds > 0 {
+            let mut first = pair[..1].to_string();
+            let mut last = pair[1..].to_string();
+            let new = rules.get(pair).unwrap().chars().next().unwrap();
+            first.push(new);
+            last.insert(0, new);
+            *counts.entry(new).or_insert(0) +=1;
+            merge(&mut counts, &apply_rec(&rules, &first, rounds-1));
+            merge(&mut counts, &apply_rec(&rules, &last, rounds-1));
+        }
+        counts
+    }
+}
+
 #[cfg(test)]
 mod test {
     use test_case::test_case;
@@ -63,64 +124,5 @@ mod test {
         let (most, least) = count_most_least(counts);
 
         most - least
-    }
-
-    fn merge(counts: &mut HashMap<char, usize>, more: &HashMap<char, usize>) {
-        for (c, n) in more {
-            *counts.entry(*c).or_insert(0) += *n;
-        }
-    }
-
-    cached_key! {
-        FOO: UnboundCache<String, HashMap<char, usize>> = UnboundCache::new();
-        Key = { format!("{}{}{}", pair, rounds, rules.len())};
-        fn apply_rec(rules: &HashMap<String, String>, pair: &str, rounds: i32) -> HashMap<char, usize> = {
-            let mut counts = HashMap::new();
-            if rounds > 0 {
-                let mut first = pair[..1].to_string();
-                let mut last = pair[1..].to_string();
-                let new = rules.get(pair).unwrap().chars().next().unwrap();
-                first.push(new);
-                last.insert(0, new);
-                *counts.entry(new).or_insert(0) +=1;
-                merge(&mut counts, &apply_rec(&rules, &first, rounds-1));
-                merge(&mut counts, &apply_rec(&rules, &last, rounds-1));
-            }
-            counts
-        }
-    }
-
-    fn count_most_least(counts: HashMap<char, usize>) -> (usize, usize) {
-        let counts = counts;
-        (
-            *counts.values().max().unwrap(),
-            *counts.values().min().unwrap(),
-        )
-    }
-
-    fn count(polymer: String) -> HashMap<char, usize> {
-        let counts = polymer.chars().fold(HashMap::new(), |mut m, c| {
-            *m.entry(c).or_insert(0) += 1;
-            m
-        });
-        counts
-    }
-
-    fn apply(template: String, rules: &HashMap<String, String>) -> String {
-        let mut polymer = template[..1].to_string();
-
-        for i in 0..template.len() - 1 {
-            let pair = &template[i..=i + 1];
-            let last = &pair[1..];
-            match rules.get(pair) {
-                Some(insert) => {
-                    polymer += insert;
-                    polymer += last
-                }
-                None => polymer += last,
-            }
-        }
-
-        polymer
     }
 }
