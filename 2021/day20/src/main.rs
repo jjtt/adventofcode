d293b3dfb285dbef852fd97091d3dcbd3c27310c
jjtt <3908945+jjtt@users.noreply.github.com>
2022-01-12 11:42:03 +0200

    Sample passing, but 5510 is too high for input

--- 2021/day20/src/main.rs
+++ 2021/day20/src/main.rs
@@ -29,8 +29,8 @@ fn parse_input(input: &str) -> (Vec<bool>, HashSet<(i32, i32)>) {
     (algorithm, image)
 }
 
-fn neighbours_to_index(image: &HashSet<(i32, i32)>, x: i32, y: i32) -> usize {
-    ((image.contains(&(x - 1, y - 1)) as usize) << 8)
+fn neighbours_to_index(image: &HashSet<(i32, i32)>, x: i32, y: i32, negative: bool) -> usize {
+    let index = ((image.contains(&(x - 1, y - 1)) as usize) << 8)
         + ((image.contains(&(x, y - 1)) as usize) << 7)
         + ((image.contains(&(x + 1, y - 1)) as usize) << 6)
         + ((image.contains(&(x - 1, y)) as usize) << 5)
@@ -38,40 +38,56 @@ fn neighbours_to_index(image: &HashSet<(i32, i32)>, x: i32, y: i32) -> usize {
         + ((image.contains(&(x + 1, y)) as usize) << 3)
         + ((image.contains(&(x - 1, y + 1)) as usize) << 2)
         + ((image.contains(&(x, y + 1)) as usize) << 1)
-        + ((image.contains(&(x + 1, y + 1)) as usize) << 0)
+        + ((image.contains(&(x + 1, y + 1)) as usize) << 0);
+
+    if negative {
+        (!index) & 0b111111111
+    } else {
+        index
+    }
 }
 
-fn apply(algo: &Vec<bool>, image: HashSet<(i32, i32)>) -> HashSet<(i32, i32)> {
-    let x_min = image.iter().map(|(x, _)| x).min().unwrap();
-    let x_max = image.iter().map(|(x, _)| x).max().unwrap();
-    let y_min = image.iter().map(|(_, y)| y).min().unwrap();
-    let y_max = image.iter().map(|(_, y)| y).max().unwrap();
-
+fn apply(
+    algo: &Vec<bool>,
+    image: HashSet<(i32, i32)>,
+    min: i32,
+    max: i32,
+    negative: bool,
+) -> (HashSet<(i32, i32)>, bool) {
     let mut out = HashSet::new();
 
-    for x in x_min - 1..=x_max + 1 {
-        for y in y_min - 1..=y_max + 1 {
-            if algo[neighbours_to_index(&image, x, y)] {
+    let makes_negatives = *algo.first().unwrap();
+
+    for x in min - 1..=max + 1 {
+        for y in min - 1..=max + 1 {
+            let new_value = algo[neighbours_to_index(&image, x, y, negative)];
+            if !makes_negatives && new_value {
+                out.insert((x, y));
+            } else if makes_negatives && !new_value {
                 out.insert((x, y));
             }
         }
     }
 
-    out
+    (out, makes_negatives)
 }
 
-fn print(image: &HashSet<(i32, i32)>) {
-    let x_min = image.iter().map(|(x, _)| x).min().unwrap();
-    let x_max = image.iter().map(|(x, _)| x).max().unwrap();
-    let y_min = image.iter().map(|(_, y)| y).min().unwrap();
-    let y_max = image.iter().map(|(_, y)| y).max().unwrap();
-
-    for y in *y_min..=*y_max {
-        for x in *x_min..=*x_max {
-            if image.contains(&(x, y)) {
-                print!("#");
+fn print(image: &HashSet<(i32, i32)>, min: i32, max: i32, negative: bool) {
+    for y in min..=max {
+        for x in min..=max {
+            let contains = image.contains(&(x, y));
+            if contains {
+                if negative {
+                    print!(".");
+                } else {
+                    print!("#");
+                }
             } else {
-                print!(".");
+                if negative {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
             }
         }
         println!();
@@ -111,27 +127,33 @@ mod test {
     fn index_for_center_in_sample() {
         let (_, image) = parse_input("sample1.txt");
 
-        assert_eq!(34, neighbours_to_index(&image, 2, 2));
+        assert_eq!(34, neighbours_to_index(&image, 2, 2, false));
+        assert_eq!(477, neighbours_to_index(&image, 2, 2, true));
     }
 
     #[test]
     fn indices_for_input() {
         let (_, image) = parse_input("input.txt");
 
-        assert_eq!(431, neighbours_to_index(&image, 98, 98));
-        assert_eq!(31, neighbours_to_index(&image, 1, 1));
+        assert_eq!(431, neighbours_to_index(&image, 98, 98, false));
+        assert_eq!(31, neighbours_to_index(&image, 1, 1, false));
     }
 
     #[test_case("sample1.txt" => is eq(35); "sample1")]
+    #[test_case("sample2.txt" => is eq(0); "sample2")]
     #[test_case("input.txt" => is eq(0); "input")]
     fn part1(input: &str) -> usize {
         let (algo, image) = parse_input(input);
 
-        print(&image);
-        let image = apply(&algo, image);
-        print(&image);
-        let image = apply(&algo, image);
-        print(&image);
+        let min = *image.iter().map(|(x, _)| x).min().unwrap();
+        let max = *image.iter().map(|(x, _)| x).max().unwrap();
+
+        let negative = false;
+        print(&image, min, max, negative);
+        let (image, negative) = apply(&algo, image, min, max, negative);
+        print(&image, min - 1, max + 1, negative);
+        let (image, negative) = apply(&algo, image, min - 1, max + 1, negative);
+        print(&image, min - 2, max + 2, negative);
 
         image.len()
     }
