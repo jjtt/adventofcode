7496b5a09871b9548f5ef3c91336854049b881af
jjtt <3908945+jjtt@users.noreply.github.com>
2022-01-16 11:47:01 +0200

    Printing states, generating moves

--- 2021/day23/src/main.rs
+++ 2021/day23/src/main.rs
@@ -1,18 +1,19 @@
 #[macro_use]
 extern crate scan_fmt;
-use itertools::Itertools;
+
+use itertools::{all, Itertools};
+use std::cmp;
 use std::collections::HashSet;
 
 use std::fs::read_to_string;
 use std::ops::Range;
 
-fn parse_situation(input: &str) -> () {
-    let s = read_to_string(input).unwrap();
+fn parse_situation(s: String) -> [char; 19] {
     let mut lines = s.lines();
 
     assert_eq!("#############", lines.next().unwrap());
-    assert_eq!("#...........#", lines.next().unwrap());
 
+    let hallway = scan_fmt!(lines.next().unwrap(), "#{}}#", String).unwrap();
     let top = scan_fmt!(
         lines.next().unwrap(),
         "###{}#{}#{}#{}###",
@@ -33,6 +34,255 @@ fn parse_situation(input: &str) -> () {
     .unwrap();
 
     assert_eq!("  #########", lines.next().unwrap());
+
+    let mut hall = hallway[0..11].chars();
+
+    [
+        bottom.0,
+        top.0,
+        bottom.1,
+        top.1,
+        bottom.2,
+        top.2,
+        bottom.3,
+        top.3,
+        hall.next().unwrap(),
+        hall.next().unwrap(),
+        hall.next().unwrap(),
+        hall.next().unwrap(),
+        hall.next().unwrap(),
+        hall.next().unwrap(),
+        hall.next().unwrap(),
+        hall.next().unwrap(),
+        hall.next().unwrap(),
+        hall.next().unwrap(),
+        hall.next().unwrap(),
+    ]
+}
+
+fn print(state: [char; 19]) -> String {
+    let mut s = String::new();
+
+    s += "#############\n";
+    s += "#";
+    s.extend(state[8..19].iter());
+    s += "#\n";
+    s += format!(
+        "###{}#{}#{}#{}###\n",
+        state[1], state[3], state[5], state[7]
+    )
+    .as_str();
+    s += format!("  #{}#{}#{}#{}#\n", state[0], state[2], state[4], state[6]).as_str();
+    s += "  #########\n";
+
+    s
+}
+
+fn is_finished(state: &[char; 19]) -> bool {
+    *state
+        == [
+            'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+            '.', '.',
+        ]
+}
+
+fn find_moves(state: [char; 19]) -> Vec<[char; 19]> {
+    let pods = amphipods(&state);
+
+    let mut moves = vec![];
+
+    for (c, pos) in pods {
+        moves.extend(moves_for(c, pos, &state));
+    }
+
+    moves
+}
+
+fn moves_for(amphipod_type: char, pos: usize, state: &[char; 19]) -> Vec<[char; 19]> {
+    if is_final_position_for(amphipod_type, pos, state) {
+        return vec![];
+    }
+
+    let mut moves = vec![];
+
+    if pos >= 8 {
+        // moving in
+        let (back, front) = match amphipod_type {
+            'A' => (0, 1),
+            'B' => (2, 3),
+            'C' => (4, 5),
+            'D' => (6, 7),
+            _ => panic!("Unsupported amphipod type: {}", amphipod_type),
+        };
+        if is_allowed_move_for(amphipod_type, pos, back, state) {
+            let mut m = state.clone();
+            m[pos] = '.';
+            m[back] = amphipod_type;
+            moves.push(m);
+            println!("{}", print(m));
+        }
+
+        if is_allowed_move_for(amphipod_type, pos, front, state) {
+            let mut m = state.clone();
+            m[pos] = '.';
+            m[front] = amphipod_type;
+            moves.push(m);
+            println!("{}", print(m));
+        }
+    } else {
+        // moving out
+        let allowed_hallway_positions = vec![8, 9, 11, 13, 15, 17, 18];
+        for hall_pos in allowed_hallway_positions {
+            if is_allowed_move_for(amphipod_type, pos, hall_pos, state) {
+                let mut m = state.clone();
+                m[pos] = '.';
+                m[hall_pos] = amphipod_type;
+                moves.push(m);
+                println!("{}", print(m));
+            }
+        }
+    }
+
+    moves
+}
+
+fn is_final_position_for(amphipod_type: char, pos: usize, state: &[char; 19]) -> bool {
+    match pos {
+        0 => 'A' == amphipod_type,
+        1 => 'A' == amphipod_type && state[0] == 'A',
+        2 => 'B' == amphipod_type,
+        3 => 'B' == amphipod_type && state[2] == 'B',
+        4 => 'C' == amphipod_type,
+        5 => 'C' == amphipod_type && state[4] == 'C',
+        6 => 'D' == amphipod_type,
+        7 => 'D' == amphipod_type && state[6] == 'D',
+        _ => false,
+    }
+}
+
+fn is_allowed_move_for(
+    amphipod_type: char,
+    start_pos: usize,
+    target_pos: usize,
+    state: &[char; 19],
+) -> bool {
+    // doorways are always clear
+    assert!(state[10] == '.' && state[12] == '.' && state[14] == '.' && state[16] == '.');
+
+    // target must be clear
+    if state[target_pos] != '.' {
+        return false;
+    }
+
+    if !room_matches_type(amphipod_type, target_pos) {
+        return false;
+    }
+
+    if wrong_type_in_room(target_pos, state) {
+        return false;
+    }
+
+    let (s, t) = if start_pos < 8 {
+        (start_pos, target_pos)
+    } else {
+        (target_pos, start_pos)
+    };
+
+    // can not move in/out?
+    if back_of_room_blocked(s, state) {
+        return false;
+    }
+
+    let steps_out = 2 - (s % 2);
+
+    let out = match s {
+        0 | 1 => 10,
+        2 | 3 => 12,
+        4 | 5 => 14,
+        6 | 7 => 16,
+        _ => panic!(
+            "Start or end must be in a room: {} (start:{}, end:{})",
+            s, start_pos, target_pos
+        ),
+    };
+
+    (cmp::min(out, t)..cmp::max(out, t))
+        .skip(1)
+        .all(|p| state[p] == '.')
+}
+
+fn wrong_type_in_room(pos: usize, state: &[char; 19]) -> bool {
+    match pos {
+        1 => state[0] != 'A',
+        3 => state[2] != 'B',
+        5 => state[4] != 'C',
+        7 => state[6] != 'D',
+        _ => false,
+    }
+}
+
+fn room_matches_type(amphipod_type: char, pos: usize) -> bool {
+    match pos {
+        0 | 1 => 'A' == amphipod_type,
+        2 | 3 => 'B' == amphipod_type,
+        4 | 5 => 'C' == amphipod_type,
+        6 | 7 => 'D' == amphipod_type,
+        _ => true,
+    }
+}
+
+fn back_of_room_blocked(pos: usize, state: &[char; 19]) -> bool {
+    (pos == 0 && state[1] != '.')
+        || (pos == 2 && state[3] != '.')
+        || (pos == 4 && state[5] != '.')
+        || (pos == 6 && state[7] != '.')
+}
+
+fn amphipods(state: &[char; 19]) -> [(char, usize); 8] {
+    let mut pods = [(' ', 0); 8];
+    let mut seen_a = false;
+    let mut seen_b = false;
+    let mut seen_c = false;
+    let mut seen_d = false;
+    for (i, c) in state.iter().enumerate() {
+        match c {
+            'A' => {
+                if seen_a {
+                    pods[1] = (*c, i)
+                } else {
+                    pods[0] = (*c, i);
+                    seen_a = true
+                }
+            }
+            'B' => {
+                if seen_b {
+                    pods[3] = (*c, i)
+                } else {
+                    pods[2] = (*c, i);
+                    seen_b = true
+                }
+            }
+            'C' => {
+                if seen_c {
+                    pods[5] = (*c, i)
+                } else {
+                    pods[4] = (*c, i);
+                    seen_c = true
+                }
+            }
+            'D' => {
+                if seen_d {
+                    pods[7] = (*c, i)
+                } else {
+                    pods[6] = (*c, i);
+                    seen_d = true
+                }
+            }
+            '.' => (),
+            _ => panic!("Unexpected character: {}", c),
+        }
+    }
+    pods
 }
 
 fn main() {
@@ -41,14 +291,106 @@ fn main() {
 
 #[cfg(test)]
 mod test {
+    use indoc::indoc;
     use test_case::test_case;
 
     use super::*;
 
+    #[test]
+    fn print_sample1() {
+        let state = parse_situation(read_to_string("sample1.txt").unwrap());
+
+        assert_eq!(read_to_string("sample1.txt").unwrap(), print(state));
+    }
+
+    #[test]
+    fn only_one_possible_move_in_for_a() {
+        let state = parse_situation(
+            indoc! {"
+                #############
+                #A..........#
+                ###.#B#C#D###
+                  #A#B#C#D#
+                  #########
+        "}
+            .to_string(),
+        );
+
+        let moves = find_moves(state);
+
+        assert_eq!(1, moves.len());
+
+        assert!(is_finished(moves.first().unwrap()));
+    }
+
+    #[test]
+    fn only_one_possible_move_in_for_d() {
+        let state = parse_situation(
+            indoc! {"
+                #############
+                #.......D...#
+                ###A#B#C#.###
+                  #A#B#C#D#
+                  #########
+        "}
+            .to_string(),
+        );
+
+        let moves = find_moves(state);
+
+        assert_eq!(1, moves.len());
+
+        assert!(is_finished(moves.first().unwrap()));
+    }
+
+    #[test]
+    fn two_possible_moves_out_for_d() {
+        let state = parse_situation(
+            indoc! {"
+                #############
+                #...A.B.C.D.#
+                ###.#.#.#.###
+                  #D#C#B#A#
+                  #########
+        "}
+            .to_string(),
+        );
+
+        let moves = find_moves(state);
+
+        assert_eq!(2, moves.len());
+
+        assert!(!is_finished(moves.first().unwrap()));
+        assert!(!is_finished(moves.last().unwrap()));
+        assert_ne!(moves.first().unwrap(), moves.last().unwrap());
+    }
+
+    #[test]
+    fn two_possible_moves_out_for_c_and_d() {
+        let state = parse_situation(
+            indoc! {"
+                #############
+                #.A...B.C.D.#
+                ###.#.#.#.###
+                  #D#C#B#A#
+                  #########
+        "}
+            .to_string(),
+        );
+
+        let moves = find_moves(state);
+
+        assert_eq!(2, moves.len());
+
+        assert!(!is_finished(moves.first().unwrap()));
+        assert!(!is_finished(moves.last().unwrap()));
+        assert_ne!(moves.first().unwrap(), moves.last().unwrap());
+    }
+
     #[test_case("sample1.txt" => is eq(12521); "sample1")]
     #[test_case("input.txt" => is eq(0); "input")]
     fn part1(input: &str) -> usize {
-        let steps = parse_situation(input);
+        let state = parse_situation(read_to_string(input).unwrap());
 
         0
     }
