68f1169188cd56947dc0452b2e8cde3ab1d3d444
jjtt <3908945+jjtt@users.noreply.github.com>
2022-01-24 20:56:30 +0200

    Animate the cucumber dance

--- 2021/day25/src/main.rs
+++ 2021/day25/src/main.rs
@@ -1,4 +1,8 @@
+use std::env;
 use std::fs::read_to_string;
+use std::io::{stdout, Write};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn parse_cucumber_map(input: String) -> (Vec<bool>, Vec<bool>, usize, usize) {
     (
@@ -57,8 +61,49 @@ fn next_south(pos: usize, size_east: usize, size_south: usize) -> usize {
     ((pos / size_east + 1) % size_south) * size_east + pos % size_east
 }
 
+fn part1(input: &str, do_print: bool) -> usize {
+    let (mut east, mut south, size_east, size_south) =
+        parse_cucumber_map(read_to_string(input).unwrap());
+
+    let mut moves = 0;
+    let mut moved = true;
+    while moved {
+        moves += 1;
+
+        if do_print {
+            print(&east, &south, size_east)
+        };
+        moved = do_the_cucumber_dance(&mut east, &mut south, size_east, size_south);
+    }
+    if do_print {
+        print(&east, &south, size_east)
+    };
+
+    moves
+}
+
+fn print(east: &Vec<bool>, south: &Vec<bool>, size_east: usize) {
+    print!("{esc}c", esc = 27 as char);
+    for i in 0..east.len() {
+        let c = if east[i] {
+            '>'
+        } else if south[i] {
+            'v'
+        } else {
+            '.'
+        };
+        print!("{}", c);
+        if (i + 1) % size_east == 0 {
+            println!()
+        }
+    }
+    stdout().flush().unwrap();
+    sleep(Duration::from_millis(20));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = env::args().nth(1).unwrap();
+    part1(&input, true);
 }
 
 #[cfg(test)]
@@ -90,17 +135,6 @@ mod test {
     #[test_case("sample1.txt" => is eq(58); "sample1")]
     #[test_case("input.txt" => is eq(414); "input")]
     fn part1(input: &str) -> usize {
-        let (mut east, mut south, size_east, size_south) =
-            parse_cucumber_map(read_to_string(input).unwrap());
-
-        let mut moves = 0;
-        let mut moved = true;
-        while moved {
-            moves += 1;
-
-            moved = do_the_cucumber_dance(&mut east, &mut south, size_east, size_south);
-        }
-
-        moves
+        super::part1(input, false)
     }
 }
