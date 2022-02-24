d759e05ea9a220875e71ac1351b98edcb77cd2b0
jjtt <3908945+jjtt@users.noreply.github.com>
2022-02-24 22:06:40 +0200

    Day10 part2, duh

--- 2015/10/part2.py
+++ 2015/10/part2.py
@@ -20,18 +20,7 @@ assert play("1211") == "111221"
 assert play("111221") == "312211"
 
 value = "1113222113"
-for _ in range(40):
+for _ in range(50):
     value = play(value)
 
-conway = 1.303577269
-
-print(len(value)*int(conway**10))
-
-# 3536316 is too low
-
-value = len(value)
-for _ in range(10):
-    value = int(value * conway)
-    print(value)
-
-# 3579213 is too low
+print(len(value))
