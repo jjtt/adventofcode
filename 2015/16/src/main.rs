d46800687a05c20cd71d0529fac1ecbf48ca56f8
jjtt <3908945+jjtt@users.noreply.github.com>
2022-04-03 21:19:46 +0300

    Day16 part2

--- 2015/16/src/main.rs
+++ 2015/16/src/main.rs
@@ -2,7 +2,7 @@ use single::Single;
 use std::collections::HashMap;
 use std::fs::read_to_string;
 
-fn filter(sue: &HashMap<String, i32>, evidence: &HashMap<String, i32>) -> bool {
+fn filter_part1(sue: &HashMap<String, i32>, evidence: &HashMap<String, i32>) -> bool {
     for (e, n) in evidence {
         if sue.contains_key(e) {
             if sue[e] != *n {
@@ -13,17 +13,27 @@ fn filter(sue: &HashMap<String, i32>, evidence: &HashMap<String, i32>) -> bool {
     true
 }
 
-fn main() {
-    let evidence: HashMap<String, i32> = read_to_string("evidence.txt")
-        .unwrap()
-        .lines()
-        .map(|l| l.split_once(": ").unwrap())
-        .map(|(e, n)| (e.to_string(), n.parse::<i32>().unwrap()))
-        .collect();
-    dbg!(&evidence);
+fn filter_part2(sue: &HashMap<String, i32>, evidence: &HashMap<String, i32>) -> bool {
+    for (e, n) in evidence {
+        if sue.contains_key(e) {
+            if match e.as_str() {
+                "cats" | "trees" => sue[e] <= *n,
+                "pomeranians" | "goldfish" => sue[e] >= *n,
+                _ => sue[e] != *n,
+            } {
+                return false;
+            }
+        }
+    }
+    true
+}
 
-    let input = read_to_string("input.txt").unwrap();
-    let sue = input
+fn filter_sues(
+    input: String,
+    evidence: &HashMap<String, i32>,
+    filter: fn(&HashMap<String, i32>, &HashMap<String, i32>) -> bool,
+) -> (i32, HashMap<String, i32>) {
+    input
         .lines()
         .map(|l| l.split_once(": ").unwrap())
         .map(|(s, p)| {
@@ -36,7 +46,24 @@ fn main() {
             )
         })
         .filter(|(_, s)| filter(&s, &evidence))
-        .single();
+        .single()
+        .unwrap()
+}
 
-    dbg!(sue);
+fn main() {
+    let evidence: HashMap<String, i32> = read_to_string("evidence.txt")
+        .unwrap()
+        .lines()
+        .map(|l| l.split_once(": ").unwrap())
+        .map(|(e, n)| (e.to_string(), n.parse::<i32>().unwrap()))
+        .collect();
+
+    let input = read_to_string("input.txt").unwrap();
+    let sue = filter_sues(input.clone(), &evidence, filter_part1);
+
+    println!("Part 1 - Sue {}", sue.0);
+
+    let sue = filter_sues(input, &evidence, filter_part2);
+
+    println!("Part 2 - Sue {}", sue.0);
 }
