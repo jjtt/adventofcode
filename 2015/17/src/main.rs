e52bb31377a18abed43433fc67dec64327750b31
jjtt <3908945+jjtt@users.noreply.github.com>
2022-12-01 14:15:01 +0200

    Day17 part2

--- 2015/17/src/main.rs
+++ 2015/17/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 
     println!("Test - {} combinations", combinations(25, &test_containers));
     println!("Part1 - {} combinations", combinations(150, &containers));
+    println!("Part2 - {} minimum ways", min_ways(150, &containers));
 }
 
 fn combinations(nog: i32, containers: &Vec<i32>) -> usize {
@@ -24,4 +25,24 @@ fn combinations(nog: i32, containers: &Vec<i32>) -> usize {
                 .count()
         })
         .sum()
+}
+
+fn min_ways(nog: i32, containers: &Vec<i32>) -> usize {
+    let mut ways = (1..containers.len())
+        .flat_map(|size| {
+            containers
+                .iter()
+                .combinations(size)
+                .filter(|combination| nog == combination.iter().copied().sum())
+                .collect::<Vec<Vec<&i32>>>()
+        })
+        .collect::<Vec<Vec<&i32>>>();
+    ways.sort_by_key(Vec::len);
+    let min = ways.first().unwrap().len();
+    for (index, way) in ways.iter().enumerate() {
+        if way.len() != min {
+            return index;
+        }
+    }
+    panic!("Not found?")
 }
