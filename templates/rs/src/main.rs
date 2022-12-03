c92e647c7a69913ceaf32b2cc72a2aeb05e896cc
jjtt <3908945+jjtt@users.noreply.github.com>
2022-12-03 18:39:05 +0200

    Solve, measure and print

--- templates/rs/src/main.rs
+++ templates/rs/src/main.rs
@@ -1,10 +1,28 @@
 use crate::solution::{part1, part2};
+use std::time::Instant;
 
 mod solution;
+
+#[derive(Debug)]
+enum Part {
+    One,
+    Two,
+}
+
+fn solve_measure_and_print(part: Part, input: &str) {
+    let solver = match part {
+        Part::One => part1,
+        Part::Two => part2,
+    };
+    let start = Instant::now();
+    let result = solver(input);
+    let micros = start.elapsed().as_micros();
+    println!("Part {part:?}: {result} ({micros}µs)");
+}
 
 fn main() {
     const NAME: &str = env!("CARGO_PKG_NAME");
     println!("{NAME}:");
-    println!("part1: {}", part1("input.txt"));
-    println!("part2: {}", part2("input.txt"));
+    solve_measure_and_print(Part::One, "input.txt");
+    solve_measure_and_print(Part::Two, "input.txt");
 }
