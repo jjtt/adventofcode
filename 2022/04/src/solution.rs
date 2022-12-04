f9c2a16de7db3942b6024efe6aeb3428f2a24035
jjtt <3908945+jjtt@users.noreply.github.com>
2022-12-04 11:30:35 +0200

    Day4 part1

--- 2022/04/src/solution.rs
+++ 2022/04/src/solution.rs
@@ -5,10 +5,7 @@ use std::ops::RangeInclusive;
 
 pub fn part1(input: &str) -> usize {
     let input = read_to_string(input).unwrap();
-    input.lines()
-        .map(parse_row)
-        .filter(contains)
-        .count()
+    input.lines().map(parse_row).filter(contains).count()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -19,12 +16,15 @@ pub fn part2(input: &str) -> usize {
 fn parse_row(row: &str) -> (RangeInclusive<usize>, RangeInclusive<usize>) {
     let (start1, end1, start2, end2) =
         scan_fmt!(row, "{d}-{d},{d}-{d}", usize, usize, usize, usize).unwrap();
+    assert!(start1 <= end1);
+    assert!(start2 <= end2);
     (start1..=end1, start2..=end2)
 }
 
 fn contains(ranges: &(RangeInclusive<usize>, RangeInclusive<usize>)) -> bool {
-    let mut ranges = ranges.clone();
-    ranges.0.all(|v| ranges.1.contains(&v)) || ranges.1.all(|v| ranges.0.contains(&v))
+    let mut ranges1 = ranges.clone();
+    let mut ranges2 = ranges.clone();
+    ranges1.0.all(|v| ranges1.1.contains(&v)) || ranges2.1.all(|v| ranges2.0.contains(&v))
 }
 
 #[cfg(test)]
@@ -35,12 +35,14 @@ mod tests {
     fn parsing_rows() {
         assert_eq!((2..=8, 3..=7), parse_row("2-8,3-7"));
         assert_eq!((20..=80, 300..=700), parse_row("20-80,300-700"));
+        assert_eq!((8..=82, 3..=96), parse_row("8-82,3-96"));
     }
 
     #[test]
     fn containing() {
         assert!(contains(&(2..=8, 3..=7)));
         assert!(contains(&(3..=7, 2..=8)));
+        assert!(contains(&(8..=82, 3..=96)));
         assert!(!contains(&(3..=7, 1..=5)));
         assert!(!contains(&(0..=1, 2..=3)));
         assert!(contains(&(1..=1, 1..=1)));
