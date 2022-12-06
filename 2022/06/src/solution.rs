3987ce19d11c4b301ccf0ba3e76527ae8287fab0
jjtt <3908945+jjtt@users.noreply.github.com>
2022-12-06 09:30:10 +0200

    Day6, part2

--- 2022/06/src/solution.rs
+++ 2022/06/src/solution.rs
@@ -1,32 +1,32 @@
-use anyhow::bail;
-use scan_fmt::scan_fmt;
 use std::collections::VecDeque;
 use std::fs::read_to_string;
 
 pub fn part1(input: &str) -> usize {
     let input = read_to_string(input).unwrap();
     let signal = input.lines().next().unwrap();
-    find_marker(signal)
+    find_marker(signal, 4)
 }
 
 pub fn part2(input: &str) -> usize {
-    //todo!()
-    0
+    let input = read_to_string(input).unwrap();
+    let signal = input.lines().next().unwrap();
+    find_marker(signal, 14)
 }
 
-fn find_marker(signal: &str) -> usize {
+fn find_marker(signal: &str, num: usize) -> usize {
     let mut remaining = signal.chars();
     let mut buffer = VecDeque::new();
     buffer.push_back(0 as char);
-    buffer.push_back(remaining.next().unwrap());
-    buffer.push_back(remaining.next().unwrap());
-    buffer.push_back(remaining.next().unwrap());
+    for _ in 0..num - 1 {
+        buffer.push_back(remaining.next().unwrap());
+    }
     for (index, c) in remaining.enumerate() {
         buffer.pop_front();
         buffer.push_back(c);
         let buf = buffer.clone();
+        assert_eq!(num, buf.len());
         if !has_duplicates(buf) {
-            return index + 4;
+            return index + num;
         }
     }
 
@@ -61,14 +61,28 @@ mod tests {
 
     #[test]
     fn samples() {
-        assert_eq!(5, find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"));
-        assert_eq!(6, find_marker("nppdvjthqldpwncqszvftbrmjlhg"));
-        assert_eq!(10, find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
-        assert_eq!(11, find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+        assert_eq!(5, find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4));
+        assert_eq!(6, find_marker("nppdvjthqldpwncqszvftbrmjlhg", 4));
+        assert_eq!(10, find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4));
+        assert_eq!(11, find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4));
+    }
+
+    #[test]
+    fn samples2() {
+        assert_eq!(19, find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14));
+        assert_eq!(23, find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14));
+        assert_eq!(23, find_marker("nppdvjthqldpwncqszvftbrmjlhg", 14));
+        assert_eq!(29, find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14));
+        assert_eq!(26, find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14));
     }
 
     #[test]
     fn part1_sample() {
         assert_eq!(7, part1("sample.txt"));
+    }
+
+    #[test]
+    fn part2_sample() {
+        assert_eq!(19, part2("sample.txt"));
     }
 }
