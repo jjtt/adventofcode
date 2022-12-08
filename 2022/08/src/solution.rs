ad3667bb9ce321141665985221f714f884c3a29b
jjtt <3908945+jjtt@users.noreply.github.com>
2022-12-08 15:49:43 +0200

    Day8, part1

--- 2022/08/src/solution.rs
+++ 2022/08/src/solution.rs
@@ -1,10 +1,9 @@
-use anyhow::bail;
-use scan_fmt::scan_fmt;
+use itertools::Itertools;
+use ndarray::{s, Array2};
 use std::fs::read_to_string;
 
 pub fn part1(input: &str) -> usize {
-    //todo!()
-    0
+    count_visible(&parse(&read_to_string(input).unwrap()))
 }
 
 pub fn part2(input: &str) -> usize {
@@ -12,10 +11,56 @@ pub fn part2(input: &str) -> usize {
     0
 }
 
+fn visible(trees: &Array2<u8>, row: usize, column: usize) -> bool {
+    let current = trees[(row, column)];
+    trees
+        .row(row)
+        .slice(s![..column])
+        .iter()
+        .all(|v| *v < current)
+        || trees
+            .row(row)
+            .slice(s![(column + 1)..])
+            .iter()
+            .all(|v| *v < current)
+        || trees
+            .column(column)
+            .slice(s![..row])
+            .iter()
+            .all(|v| *v < current)
+        || trees
+            .column(column)
+            .slice(s![(row + 1)..])
+            .iter()
+            .all(|v| *v < current)
+}
+
+fn count_visible(trees: &Array2<u8>) -> usize {
+    let rows = trees.shape()[0];
+    let columns = trees.shape()[1];
+
+    (0..rows)
+        .cartesian_product(0..columns)
+        .filter(|(r, c)| visible(&trees, *r, *c))
+        .count()
+}
+
+fn parse(input: &str) -> Array2<u8> {
+    let lines = input.lines();
+    let rows = lines.clone().count();
+    let columns = lines.clone().next().unwrap().len();
+
+    Array2::from_shape_vec((rows, columns), lines.flat_map(parse_row).collect()).unwrap()
+}
+
+fn parse_row(row: &str) -> Vec<u8> {
+    row.chars().map(|c| c.to_digit(10).unwrap() as u8).collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ndarray::{arr1, arr2, s};
+    use ndarray::{arr1, arr2, s, Array1};
 
     #[test]
     fn array_slicing() {
@@ -72,6 +117,57 @@ mod tests {
 
         let row = trees.column(2);
         assert_eq!(arr1(&[1, 1, 3]), row);
+    }
+
+    #[test]
+    fn all_visible() {
+        let trees = arr2(&[[1, 1, 1], [1, 2, 1], [1, 2, 3]]);
+        assert_eq!(&[3, 3], trees.shape());
+
+        assert!(visible(&trees, 0, 0));
+        assert!(visible(&trees, 0, 1));
+        assert!(visible(&trees, 0, 2));
+        assert!(visible(&trees, 1, 0));
+        assert!(visible(&trees, 1, 1));
+        assert!(visible(&trees, 1, 2));
+        assert!(visible(&trees, 2, 0));
+        assert!(visible(&trees, 2, 1));
+        assert!(visible(&trees, 2, 2));
+    }
+
+    #[test]
+    fn middle_hidden() {
+        let trees = arr2(&[[1, 1, 1], [1, 0, 1], [1, 2, 3]]);
+        assert_eq!(&[3, 3], trees.shape());
+
+        assert!(visible(&trees, 0, 0));
+        assert!(visible(&trees, 0, 1));
+        assert!(visible(&trees, 0, 2));
+        assert!(visible(&trees, 1, 0));
+        assert!(!visible(&trees, 1, 1));
+        assert!(visible(&trees, 1, 2));
+        assert!(visible(&trees, 2, 0));
+        assert!(visible(&trees, 2, 1));
+        assert!(visible(&trees, 2, 2));
+    }
+
+    #[test]
+    fn counting_visible() {
+        let trees = arr2(&[[1, 1, 1], [1, 0, 1], [1, 2, 3]]);
+        assert_eq!(8, count_visible(&trees));
+
+        let trees = arr2(&[[1, 0, 1], [1, 1, 1], [1, 2, 3]]);
+        assert_eq!(9, count_visible(&trees));
+    }
+
+    #[test]
+    fn parsing_trees() {
+        assert_eq!(arr2(&[[1]]), parse("1"));
+        assert_eq!(arr2(&[[1, 2], [3, 4]]), parse("12\n34"));
+        assert_eq!(
+            arr2(&[[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+            parse("123\n456\n789")
+        );
     }
 
     #[test]
