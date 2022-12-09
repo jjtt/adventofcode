d13b8ed3e0bffc211e39d946bf324958fd66a6fa
jjtt <3908945+jjtt@users.noreply.github.com>
2022-12-09 19:22:20 +0200

    Day9, part1

--- 2022/09/src/solution.rs
+++ 2022/09/src/solution.rs
@@ -1,23 +1,159 @@
 use anyhow::bail;
 use scan_fmt::scan_fmt;
+use std::collections::HashSet;
 use std::fs::read_to_string;
 
-pub fn part1(input: &str) -> i32 {
+#[derive(PartialEq, Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn from(dir: &str) -> Direction {
+        match dir {
+            "U" => Direction::Up,
+            "D" => Direction::Down,
+            "L" => Direction::Left,
+            "R" => Direction::Right,
+            _ => panic!("Invalid input: {dir}"),
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct Move {
+    direction: Direction,
+    count: usize,
+}
+
+struct End {
+    x: i64,
+    y: i64,
+}
+
+impl End {
+    fn move_towards(&mut self, d: &Direction) {
+        match *d {
+            Direction::Up => self.y += 1,
+            Direction::Down => self.y -= 1,
+            Direction::Left => self.x -= 1,
+            Direction::Right => self.x += 1,
+        }
+    }
+
+    fn follow(&mut self, head: &End) {
+        match (head.x - self.x, head.y - self.y) {
+            (-1, -2) | (-2, -1) => {
+                self.x -= 1;
+                self.y -= 1;
+            }
+            (1, -2) | (2, -1) => {
+                self.x += 1;
+                self.y -= 1;
+            }
+            (1, 2) | (2, 1) => {
+                self.x += 1;
+                self.y += 1;
+            }
+            (-1, 2) | (-2, 1) => {
+                self.x -= 1;
+                self.y += 1;
+            }
+            (2, 0) => self.x += 1,
+            (-2, 0) => self.x -= 1,
+            (0, 2) => self.y += 1,
+            (0, -2) => self.y -= 1,
+            _ => (),
+        }
+    }
+}
+
+struct Rope {
+    head: End,
+    tail: End,
+}
+
+impl Rope {
+    fn new(x: i64, y: i64) -> Rope {
+        Rope {
+            head: End { x, y },
+            tail: End { x, y },
+        }
+    }
+
+    fn perform(&mut self, moves: &Vec<Move>) -> usize {
+        let mut tail_visited = HashSet::new();
+        for Move { direction, count } in moves {
+            for _ in 0..*count {
+                self.head.move_towards(direction);
+                self.tail.follow(&self.head);
+                tail_visited.insert((self.tail.x, self.tail.y));
+            }
+        }
+        tail_visited.len()
+    }
+}
+
+pub fn part1(input: &str) -> usize {
+    let mut rope = Rope::new(0, 0);
+    rope.perform(&parse_moves(&read_to_string(input).unwrap()))
+}
+
+pub fn part2(input: &str) -> usize {
     //todo!()
     0
 }
 
-pub fn part2(input: &str) -> i32 {
-    //todo!()
-    0
+fn parse_moves(input: &str) -> Vec<Move> {
+    input
+        .lines()
+        .map(|l| scan_fmt!(l, "{} {}", String, usize).unwrap())
+        .map(|(d, count)| Move {
+            direction: Direction::from(&d),
+            count,
+        })
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
 
     #[test]
-    fn todo() {
-        todo!()
+    fn parsing_moves() {
+        let input = "R 4";
+        let expected = vec![Move {
+            direction: Direction::Right,
+            count: 4,
+        }];
+
+        assert_eq!(expected, parse_moves(input));
+    }
+
+    #[test_case(0, 0, 0, 0 => (0, 0); "same pos")]
+    #[test_case(0, 1, 0, 0 => (0, 0); "above")]
+    #[test_case(0, -1, 0, 0 => (0, 0); "below")]
+    #[test_case(1, 1, 0, 0 => (0, 0); "ne")]
+    #[test_case(-1, -1, 0, 0 => (0, 0); "sw")]
+    #[test_case(-2, -1, 0, 0 => (-1, -1); "knight")]
+    #[test_case(2, -1, 0, 0 => (1, -1); "knight2")]
+    #[test_case(0, 2, 0, 0 => (0, 1); "two above")]
+    #[test_case(0, -2, 0, 0 => (0, -1); "two below")]
+    fn following(x1: i64, y1: i64, x2: i64, y2: i64) -> (i64, i64) {
+        let head = End { x: x1, y: y1 };
+        let mut tail = End { x: x2, y: y2 };
+
+        tail.follow(&head);
+
+        (tail.x, tail.y)
+    }
+
+    #[test]
+    fn part1_sample() {
+        assert_eq!(13, part1("sample.txt"));
     }
 }
