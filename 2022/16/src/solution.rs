b01a54ad43b4e5d27c68711abfb7c3f221341131
jjtt <3908945+jjtt@users.noreply.github.com>
2022-12-24 11:47:58 +0200

    Using arrays instead of HashMaps
    Compiles but doesn't work

--- 2022/16/src/solution.rs
+++ 2022/16/src/solution.rs
@@ -5,14 +5,29 @@ use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 use std::hash::{Hash, Hasher};
 
+const MAX_VALVES: usize = 25 * 26 + 25;
+
 #[derive(Debug)]
 struct Cave {
-    valves: HashMap<usize, Valve>,
+    valves: [Option<Valve>; MAX_VALVES],
+    neighbours: [[bool; MAX_VALVES]; MAX_VALVES],
 }
 
 impl Cave {
-    fn new(valves: HashMap<usize, Valve>) -> Cave {
-        Cave { valves }
+    fn new(mut valve_map: HashMap<usize, (Valve, Vec<usize>)>) -> Cave {
+        let mut neighbours = [[false; MAX_VALVES]; MAX_VALVES];
+        for (from, (_, to)) in &valve_map {
+            for to in to {
+                neighbours[*from][*to] = true;
+            }
+        }
+
+        let mut valves = [None; MAX_VALVES];
+        for name in 0..MAX_VALVES {
+            valves[name] = valve_map.remove(&name).map(|(valve, _)| valve);
+        }
+
+        Cave { valves, neighbours }
     }
 
     fn successors(&self, current: &SearchState) -> impl IntoIterator<Item = (SearchState, i64)> {
@@ -29,7 +44,7 @@ impl Cave {
         for current_index in current.pos.keys().clone() {
             successors = successors
                 .into_iter()
-                .flat_map(|s| s.successors_for(current_index, &self.valves))
+                .flat_map(|s| s.successors_for(current_index, self))
                 .collect();
         }
 
@@ -52,15 +67,15 @@ impl Cave {
             ]),
             _ => todo!(),
         };
+        let mut open = [false; MAX_VALVES];
+        for name in 0..MAX_VALVES {
+            open[name] = self.valves[name].map(|v| v.flow_rate == 0).unwrap_or(true);
+        }
+
         let result = astar(
             &SearchState {
                 pos,
-                open: self
-                    .valves
-                    .iter()
-                    .filter(|(_, v)| v.flow_rate == 0)
-                    .map(|(n, _)| *n)
-                    .collect(),
+                open,
                 time,
                 released_pressure: 0,
                 trail: vec![],
@@ -74,14 +89,13 @@ impl Cave {
     }
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Copy, Clone)]
 struct Valve {
     flow_rate: usize,
-    tunnels: Vec<usize>,
 }
 
 impl Valve {
-    fn from(input: &str) -> (usize, Valve) {
+    fn from(input: &str) -> (usize, (Valve, Vec<usize>)) {
         let input = input
             .replace("tunnels", "tunnel")
             .replace("leads", "lead")
@@ -97,14 +111,22 @@ impl Valve {
 
         let tunnels = tunnels.split(", ").map(name_to_int).collect();
 
-        (name_to_int(&name), Valve { flow_rate, tunnels })
+        (name_to_int(&name), (Valve { flow_rate }, tunnels))
     }
 }
 
 fn name_to_int(name: &str) -> usize {
-    name.char_indices()
-        .map(|(i, c)| (c as usize).pow(i as u32 + 1))
-        .sum()
+    let bytes = name.as_bytes();
+    let first = bytes[0] - b'A';
+    let second = bytes[1] - b'A';
+    first as usize * 26 + second as usize
+}
+
+fn int_to_name(name: usize) -> String {
+    let first = char::from((name / 26) as u8 + b'A');
+    let second = char::from((name % 26) as u8 + b'A');
+
+    String::from_iter([first, second])
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Ord, PartialOrd)]
@@ -116,7 +138,7 @@ enum Who {
 #[derive(Clone, PartialEq, Eq, Debug)]
 struct SearchState {
     pos: HashMap<Who, (usize, Option<usize>)>,
-    open: Vec<usize>, // remember to keep sorted!
+    open: [bool; MAX_VALVES],
     time: usize,
     released_pressure: i64,
     trail: Vec<(Who, usize, usize)>,
@@ -134,22 +156,20 @@ impl Hash for SearchState {
 
 impl SearchState {
     fn done(&self, cave: &Cave) -> bool {
-        cave.valves.keys().all(|v| self.open.contains(v)) || self.time == 0
+        cave.valves
+            .iter()
+            .enumerate()
+            .filter(|(index, valve)| valve.is_some())
+            .all(|(index, valve)| self.open[index])
+            || self.time == 0
     }
 
     fn remaining(&self, cave: &Cave) -> i64 {
-        let sum: usize = cave
-            .valves
-            .iter()
-            .filter(|(name, valve)| !self.open.contains(name))
-            .map(|(name, valve)| valve.flow_rate * (self.time - 1))
-            .sum();
-        -(sum as i64)
+        -1000000
     }
 
     fn open(&mut self, worker_index: &Who, open_index: usize, flow_rate: usize) {
-        self.open
-            .insert(open_index, self.pos.get(worker_index).unwrap().0);
+        self.open[open_index] = true;
         self.pos.get_mut(worker_index).unwrap().1 = None;
         self.released_pressure += ((self.time - 1) * flow_rate) as i64;
         self.trail.push((
@@ -159,20 +179,18 @@ impl SearchState {
         ))
     }
 
-    fn successors_for(
-        mut self,
-        worker_index: &Who,
-        valves: &HashMap<usize, Valve>,
-    ) -> Vec<SearchState> {
-        let current_name = &self.pos.get(worker_index).unwrap().0;
-        let current_valve = valves
-            .get(current_name)
-            .expect("Should not escape the cave");
+    fn successors_for(mut self, worker_index: &Who, cave: &Cave) -> Vec<SearchState> {
+        let current_name = self.pos.get(worker_index).unwrap().0;
+        let current_valve = cave.valves[current_name].unwrap();
 
         let mut successors = vec![];
 
-        for next_name in current_valve.tunnels.iter() {
-            if self.pos.get(worker_index).unwrap().1 == Some(*next_name) {
+        for next_name in 0..MAX_VALVES {
+            if !cave.neighbours[current_name][next_name] {
+                continue;
+            }
+
+            if self.pos.get(worker_index).unwrap().1 == Some(next_name) {
                 continue;
             }
 
@@ -180,8 +198,8 @@ impl SearchState {
             for who in self.pos.keys() {
                 let (pos, prev) = pos.get_mut(who).unwrap();
                 if *who == *worker_index {
-                    *pos = *next_name;
-                    *prev = Some(*current_name);
+                    *pos = next_name;
+                    *prev = Some(current_name);
                 }
             }
 
@@ -195,8 +213,8 @@ impl SearchState {
         }
 
         let successor = if current_valve.flow_rate > 0 {
-            if let Err(open_index) = self.open.binary_search(current_name) {
-                self.open(worker_index, open_index, current_valve.flow_rate);
+            if !self.open[current_name] {
+                self.open(worker_index, current_name, current_valve.flow_rate);
                 Some(self)
             } else {
                 None
@@ -252,17 +270,11 @@ mod tests {
         let cave = Cave::new(HashMap::from([
             (
                 name_to_int("AA"),
-                Valve {
-                    flow_rate: 0,
-                    tunnels: vec![name_to_int("BB")],
-                },
+                (Valve { flow_rate: 0 }, vec![name_to_int("BB")]),
             ),
             (
                 name_to_int("BB"),
-                Valve {
-                    flow_rate: 13,
-                    tunnels: vec![name_to_int("AA")],
-                },
+                (Valve { flow_rate: 13 }, vec![name_to_int("AA")]),
             ),
         ]));
 
@@ -276,24 +288,18 @@ mod tests {
         let cave = Cave::new(HashMap::from([
             (
                 name_to_int("AA"),
-                Valve {
-                    flow_rate: 0,
-                    tunnels: vec![name_to_int("BB")],
-                },
+                (Valve { flow_rate: 0 }, vec![name_to_int("BB")]),
             ),
             (
                 name_to_int("BB"),
-                Valve {
-                    flow_rate: 13,
-                    tunnels: vec![name_to_int("AA"), name_to_int("CC")],
-                },
+                (
+                    Valve { flow_rate: 13 },
+                    vec![name_to_int("AA"), name_to_int("CC")],
+                ),
             ),
             (
                 name_to_int("CC"),
-                Valve {
-                    flow_rate: 2,
-                    tunnels: vec![name_to_int("BB")],
-                },
+                (Valve { flow_rate: 2 }, vec![name_to_int("BB")]),
             ),
         ]));
 
@@ -307,24 +313,18 @@ mod tests {
         let cave = Cave::new(HashMap::from([
             (
                 name_to_int("AA"),
-                Valve {
-                    flow_rate: 2,
-                    tunnels: vec![name_to_int("BB")],
-                },
+                (Valve { flow_rate: 2 }, vec![name_to_int("BB")]),
             ),
             (
                 name_to_int("BB"),
-                Valve {
-                    flow_rate: 13,
-                    tunnels: vec![name_to_int("AA"), name_to_int("CC")],
-                },
+                (
+                    Valve { flow_rate: 13 },
+                    vec![name_to_int("AA"), name_to_int("CC")],
+                ),
             ),
             (
                 name_to_int("CC"),
-                Valve {
-                    flow_rate: 100,
-                    tunnels: vec![name_to_int("BB")],
-                },
+                (Valve { flow_rate: 100 }, vec![name_to_int("BB")]),
             ),
         ]));
 
@@ -338,24 +338,18 @@ mod tests {
         let cave = Cave::new(HashMap::from([
             (
                 name_to_int("AA"),
-                Valve {
-                    flow_rate: 2,
-                    tunnels: vec![name_to_int("BB")],
-                },
+                (Valve { flow_rate: 2 }, vec![name_to_int("BB")]),
             ),
             (
                 name_to_int("BB"),
-                Valve {
-                    flow_rate: 13,
-                    tunnels: vec![name_to_int("AA"), name_to_int("CC")],
-                },
+                (
+                    Valve { flow_rate: 13 },
+                    vec![name_to_int("AA"), name_to_int("CC")],
+                ),
             ),
             (
                 name_to_int("CC"),
-                Valve {
-                    flow_rate: 100,
-                    tunnels: vec![name_to_int("BB")],
-                },
+                (Valve { flow_rate: 100 }, vec![name_to_int("BB")]),
             ),
         ]));
 
@@ -368,13 +362,34 @@ mod tests {
         assert_eq!(
             (
                 name_to_int("AA"),
-                Valve {
-                    flow_rate: 0,
-                    tunnels: vec![name_to_int("DD"), name_to_int("II"), name_to_int("BB")],
-                }
+                (
+                    Valve { flow_rate: 0 },
+                    vec![name_to_int("DD"), name_to_int("II"), name_to_int("BB")]
+                ),
             ),
             Valve::from("Valve AA has flow rate=0; tunnels lead to valves DD, II, BB")
         );
+    }
+
+    #[test]
+    fn name_to_index() {
+        assert_eq!(0, name_to_int("AA"));
+        assert_eq!(1, name_to_int("AB"));
+        assert_eq!(2, name_to_int("AC"));
+        assert_eq!(3, name_to_int("AD"));
+        assert_eq!(25 * 26 + 23, name_to_int("ZX"));
+    }
+
+    #[test]
+    fn index_to_name() {
+        assert_eq!("AA", int_to_name(0));
+        assert_eq!("BA", int_to_name(26));
+        assert_eq!("ZZ", int_to_name(25 * 26 + 25));
+    }
+
+    #[test]
+    fn name_to_index_to_name() {
+        assert_eq!("ZX", int_to_name(name_to_int("ZX")));
     }
 
     #[test]
