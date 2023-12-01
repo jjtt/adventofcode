134307c3d04c32557d751b2a969520cb56c26558
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-01 08:05:26 +0200

    Day1, part2

--- 2023/01/src/solution.rs
+++ 2023/01/src/solution.rs
@@ -24,69 +24,72 @@ pub fn part2(input: &str) -> u32 {
     let lines = input.lines();
     lines
         .map(|line| {
-            let line = replace_string_nums(line);
-
-            let nums = line
-                .chars()
-                .filter_map(|c| {
-                    c.to_digit(10).and(Some(c)).or_else(|| match c {
-                        'A' => Some('1'),
-                        'B' => Some('2'),
-                        'C' => Some('3'),
-                        'D' => Some('4'),
-                        'E' => Some('5'),
-                        'F' => Some('6'),
-                        'G' => Some('7'),
-                        'H' => Some('8'),
-                        'I' => Some('9'),
-                        _ => None,
-                    })
-                })
-                .collect::<Vec<_>>();
-            let f = nums.first().unwrap();
-            let l = nums.last().unwrap();
+            let f: char = find_first(line);
+            let l: char = find_last(line);
             format!("{f}{l}").parse::<u32>().unwrap()
         })
         .sum()
 }
 
-fn replace_string_nums(line: &str) -> String {
-    let mut out = String::new();
+fn find_last(line: &str) -> char {
+    let line = line.chars().rev().collect::<String>();
     let mut i = 0;
     while i < line.len() {
-        if line[i..].starts_with("one") {
-            out.push('A');
-            i += 3;
-        } else if line[i..].starts_with("two") {
-            out.push('B');
-            i += 3;
-        } else if line[i..].starts_with("three") {
-            out.push('C');
-            i += 5;
-        } else if line[i..].starts_with("four") {
-            out.push('D');
-            i += 4;
-        } else if line[i..].starts_with("five") {
-            out.push('E');
-            i += 4;
-        } else if line[i..].starts_with("six") {
-            out.push('F');
-            i += 3;
-        } else if line[i..].starts_with("seven") {
-            out.push('G');
-            i += 5;
-        } else if line[i..].starts_with("eight") {
-            out.push('H');
-            i += 5;
-        } else if line[i..].starts_with("nine") {
-            out.push('I');
-            i += 4;
-        } else {
-            out.push(line.chars().nth(i).unwrap());
-            i += 1;
+        let c = line.chars().nth(i).unwrap();
+        if c.is_digit(10) {
+            return c;
+        } else if line[i..].starts_with("eno") {
+            return '1';
+        } else if line[i..].starts_with("owt") {
+            return '2';
+        } else if line[i..].starts_with("eerht") {
+            return '3';
+        } else if line[i..].starts_with("ruof") {
+            return '4';
+        } else if line[i..].starts_with("evif") {
+            return '5';
+        } else if line[i..].starts_with("xis") {
+            return '6';
+        } else if line[i..].starts_with("neves") {
+            return '7';
+        } else if line[i..].starts_with("thgie") {
+            return '8';
+        } else if line[i..].starts_with("enin") {
+            return '9';
         }
+        i += 1;
     }
-    out
+    panic!("no digit found")
+}
+
+fn find_first(line: &str) -> char {
+    let mut i = 0;
+    while i < line.len() {
+        let c = line.chars().nth(i).unwrap();
+        if c.is_digit(10) {
+            return c;
+        } else if line[i..].starts_with("one") {
+            return '1';
+        } else if line[i..].starts_with("two") {
+            return '2';
+        } else if line[i..].starts_with("three") {
+            return '3';
+        } else if line[i..].starts_with("four") {
+            return '4';
+        } else if line[i..].starts_with("five") {
+            return '5';
+        } else if line[i..].starts_with("six") {
+            return '6';
+        } else if line[i..].starts_with("seven") {
+            return '7';
+        } else if line[i..].starts_with("eight") {
+            return '8';
+        } else if line[i..].starts_with("nine") {
+            return '9';
+        }
+        i += 1;
+    }
+    panic!("no digit found")
 }
 
 #[cfg(test)]
@@ -110,6 +113,6 @@ mod tests {
 
     #[test]
     fn part2_input() {
-        assert_eq!(0, part2("input.txt"));
+        assert_eq!(54265, part2("input.txt"));
     }
 }
