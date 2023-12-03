01cffedd42ed87853d91771076591879d53156b1
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-03 09:01:14 +0200

    Day03 init

---
+++ 2023/03/src/solution.rs
@@ -0,0 +1,33 @@
+use anyhow::bail;
+use scan_fmt::scan_fmt;
+use std::fs::read_to_string;
+
+pub fn part1(input: &str) -> i32 {
+    //todo!()
+    0
+}
+
+pub fn part2(input: &str) -> i32 {
+    //todo!()
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn todo() {
+        todo!()
+    }
+
+    #[test]
+    fn part1_sample() {
+        assert_eq!(4361, part1("sample.txt"));
+    }
+
+    #[test]
+    fn part1_input() {
+        assert_eq!(0, part1("input.txt"));
+    }
+}
