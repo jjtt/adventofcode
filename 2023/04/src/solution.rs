2cae78a2d304cf917597bf10fd67722a2ef9899e
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-04 18:01:11 +0200

    Day04, part1

--- 2023/04/src/solution.rs
+++ 2023/04/src/solution.rs
@@ -1,13 +1,58 @@
 use anyhow::bail;
 use scan_fmt::scan_fmt;
+use std::collections::HashSet;
 use std::fs::read_to_string;
+use std::str::FromStr;
 
-pub fn part1(input: &str) -> i32 {
-    //todo!()
-    0
+struct Card {
+    id: usize,
+    winners: HashSet<usize>,
+    my: Vec<usize>,
 }
 
-pub fn part2(input: &str) -> i32 {
+impl FromStr for Card {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (id, nums) = s.split_once(": ").expect("a card");
+        let id = scan_fmt!(id, "Card {d}", usize).expect("a card id");
+        let (winners, my) = nums.split_once(" | ").expect("winners and my");
+        let winners = winners
+            .split(' ')
+            .filter(|n| !n.is_empty())
+            .map(|n| n.parse().expect("a winner"))
+            .collect();
+        let my = my
+            .split(' ')
+            .filter(|n| !n.is_empty())
+            .map(|n| n.parse().expect("a my"))
+            .collect();
+        Ok(Card { id, winners, my })
+    }
+}
+
+impl Card {
+    fn worth(self) -> usize {
+        let count = self.my.iter().filter(|n| self.winners.contains(n)).count() as u32;
+        if count > 0 {
+            2usize.pow(count - 1)
+        } else {
+            0
+        }
+    }
+}
+
+pub fn part1(input: &str) -> usize {
+    let input = read_to_string(input).expect("input file");
+    input
+        .trim()
+        .lines()
+        .map(|l| l.parse::<Card>().expect("a card"))
+        .map(Card::worth)
+        .sum()
+}
+
+pub fn part2(input: &str) -> usize {
     //todo!()
     0
 }
@@ -17,17 +62,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn todo() {
-        todo!()
-    }
-
-    #[test]
     fn part1_sample() {
         assert_eq!(13, part1("sample.txt"));
     }
 
     #[test]
     fn part1_input() {
-        assert_eq!(0, part1("input.txt"));
+        assert_eq!(15205, part1("input.txt"));
     }
 }
