fcbd9cc07c2392c5d037e87c4ee5f247c33fc316
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-06 11:22:11 +0200

    Day05, part1

--- 2023/05/src/solution.rs
+++ 2023/05/src/solution.rs
@@ -1,10 +1,66 @@
 use anyhow::bail;
 use scan_fmt::scan_fmt;
 use std::fs::read_to_string;
+use std::str::FromStr;
+
+struct MapperEntry {
+    destination_start: usize,
+    source_start: usize,
+    length: usize,
+}
+
+struct Mapper {
+    entries: Vec<MapperEntry>,
+}
+
+impl FromStr for Mapper {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let entries = s
+            .lines()
+            .skip(1)
+            .map(|l| {
+                let (destination_start, source_start, length) =
+                    scan_fmt!(l, "{d} {d} {d}", usize, usize, usize).unwrap();
+                MapperEntry {
+                    destination_start,
+                    source_start,
+                    length,
+                }
+            })
+            .collect();
+        Ok(Mapper { entries })
+    }
+}
+
+impl Mapper {
+    fn map(&self, value: usize) -> usize {
+        for entry in &self.entries {
+            if value >= entry.source_start && value < entry.source_start + entry.length {
+                return entry.destination_start + value - entry.source_start;
+            }
+        }
+        value
+    }
+}
 
 pub fn part1(input: &str) -> usize {
-    //todo!()
-    0
+    let input = read_to_string(input).unwrap();
+    let mut blocks = input.split("\n\n");
+    let seeds = blocks.next().unwrap();
+    let mapppers = blocks
+        .map(|b| b.parse::<Mapper>().unwrap())
+        .collect::<Vec<_>>();
+
+    seeds[7..]
+        .split(' ')
+        .map(str::parse::<usize>)
+        .map(Result::unwrap)
+        .map(|s| dbg!(s))
+        .map(|s| mapppers.iter().fold(s, |s, m| m.map(s)))
+        .min()
+        .expect("a min value")
 }
 
 pub fn part2(input: &str) -> usize {
@@ -23,7 +79,7 @@ mod tests {
 
     #[test]
     fn part1_input() {
-        assert_eq!(0, part1("input.txt"));
+        assert_eq!(486613012, part1("input.txt"));
     }
 
     #[test]
