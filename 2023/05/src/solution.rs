9b15fc7d48163c37e0cbf8cc918242a293b4956e
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-06 10:53:41 +0200

    Day05 init

---
+++ 2023/05/src/solution.rs
@@ -0,0 +1,38 @@
+use anyhow::bail;
+use scan_fmt::scan_fmt;
+use std::fs::read_to_string;
+
+pub fn part1(input: &str) -> usize {
+    //todo!()
+    0
+}
+
+pub fn part2(input: &str) -> usize {
+    //todo!()
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_sample() {
+        assert_eq!(35, part1("sample.txt"));
+    }
+
+    #[test]
+    fn part1_input() {
+        assert_eq!(0, part1("input.txt"));
+    }
+
+    #[test]
+    fn part2_sample() {
+        assert_eq!(0, part2("sample.txt"));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(0, part2("input.txt"));
+    }
+}
