3cf5111afd4eff77ce06520303b3b766a6948e90
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-06 12:04:50 +0200

    WIP
    Thinking about just keeping track of the ends of ranges instead of all
    seed values. This is probably immensely more efficient, but I can't find
    the motivation to finnish it now.

--- 2023/05/src/solution.rs
+++ 2023/05/src/solution.rs
@@ -1,6 +1,6 @@
-
 use scan_fmt::scan_fmt;
 use std::fs::read_to_string;
+use std::ops::Range;
 use std::str::FromStr;
 
 struct MapperEntry {
@@ -35,13 +35,20 @@ impl FromStr for Mapper {
 }
 
 impl Mapper {
-    fn map(&self, value: usize) -> usize {
+    fn map(&self, value: Vec<Range<usize>>) -> Vec<Range<usize>> {
+        value
+            .into_iter()
+            .flat_map(|r| self.map_range(r))
+            .collect::<Vec<_>>()
+    }
+
+    fn map_range(&self, value: Range<usize>) -> Vec<Range<usize>> {
         for entry in &self.entries {
             if value >= entry.source_start && value < entry.source_start + entry.length {
                 return entry.destination_start + value - entry.source_start;
             }
         }
-        value
+        vec![value]
     }
 }
 
@@ -52,13 +59,16 @@ pub fn part1(input: &str) -> usize {
     let seeds = seeds[7..]
         .split(' ')
         .map(str::parse::<usize>)
-        .map(Result::unwrap);
+        .map(Result::unwrap)
+        .map(|s| vec![s..(s + 1)]);
     let mapppers = blocks
         .map(|b| b.parse::<Mapper>().unwrap())
         .collect::<Vec<_>>();
 
     seeds
         .map(|s| mapppers.iter().fold(s, |s, m| m.map(s)))
+        .flatten()
+        .flatten()
         .min()
         .expect("a min value")
 }
@@ -72,7 +82,7 @@ pub fn part2(input: &str) -> usize {
         .map(str::parse::<usize>)
         .map(Result::unwrap)
         .collect::<Vec<_>>();
-    let seeds = seeds.chunks(2).flat_map(|w| w[0]..(w[0] + w[1]));
+    let seeds = seeds.chunks(2).map(|w| vec![w[0]..(w[0] + w[1])]);
 
     let mapppers = blocks
         .map(|b| b.parse::<Mapper>().unwrap())
@@ -80,6 +90,8 @@ pub fn part2(input: &str) -> usize {
 
     seeds
         .map(|s| mapppers.iter().fold(s, |s, m| m.map(s)))
+        .flatten()
+        .flatten()
         .min()
         .expect("a min value")
 }
