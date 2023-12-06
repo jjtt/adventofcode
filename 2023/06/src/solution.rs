b80379020d44b3ef2432d08e06e83b4223a3b53e
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-06 10:40:40 +0200

    Day06, part1

--- 2023/06/src/solution.rs
+++ 2023/06/src/solution.rs
@@ -2,12 +2,42 @@ use anyhow::bail;
 use scan_fmt::scan_fmt;
 use std::fs::read_to_string;
 
-pub fn part1(input: &str) -> i32 {
-    //todo!()
-    0
+type Race = (usize, usize);
+
+fn parse(input: &str) -> Vec<Race> {
+    let mut lines = input.lines();
+    let time = lines.next().unwrap();
+    let distance = lines.next().unwrap();
+
+    let times = time
+        .split_whitespace()
+        .skip(1)
+        .map(str::parse::<usize>)
+        .map(Result::unwrap);
+    let distances = distance
+        .split_whitespace()
+        .skip(1)
+        .map(str::parse::<usize>)
+        .map(Result::unwrap);
+    times.zip(distances).collect()
 }
 
-pub fn part2(input: &str) -> i32 {
+fn wins(race: Race) -> usize {
+    let time = race.0;
+    let distance = race.1;
+    (0..=time)
+        .map(|t| t * (time - t))
+        .filter(|&t| t > distance)
+        .count()
+}
+
+pub fn part1(input: &str) -> usize {
+    let input = read_to_string(input).unwrap();
+    let races = parse(input.trim());
+    races.into_iter().map(wins).product()
+}
+
+pub fn part2(input: &str) -> usize {
     //todo!()
     0
 }
@@ -23,6 +53,6 @@ mod tests {
 
     #[test]
     fn part1_input() {
-        assert_eq!(0, part1("input.txt"));
+        assert_eq!(2374848, part1("input.txt"));
     }
 }
