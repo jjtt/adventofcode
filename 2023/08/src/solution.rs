0ea8f73ec3af498fc1ea91e435ae4e2cdddf318c
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-08 16:42:23 +0200

    Day08, part1

--- 2023/08/src/solution.rs
+++ 2023/08/src/solution.rs
@@ -1,10 +1,39 @@
 use anyhow::bail;
 use scan_fmt::scan_fmt;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
 pub fn part1(input: &str) -> usize {
-    //todo!()
-    0
+    let input = read_to_string(input).unwrap();
+    let mut lines = input.lines();
+    let instructions = lines.next().expect("a string").chars().cycle();
+    let lines = lines.skip(1);
+    let map = lines
+        .map(|line| {
+            let (x, y, z) =
+                scan_fmt!(line, "{} = ({},{})", String, String, String).expect("a valid line");
+            (x, (y, z))
+        })
+        .collect::<HashMap<_, _>>();
+
+    let mut loc = "AAA";
+    instructions
+        .enumerate()
+        .find_map(|(i, dir)| {
+            let options = map.get(loc).expect("a valid location");
+            loc = match dir {
+                'L' => &options.0,
+                'R' => &options.1,
+                _ => panic!("invalid direction"),
+            };
+            if loc == "ZZZ" {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .expect("to find a path")
+        + 1
 }
 
 pub fn part2(input: &str) -> usize {
@@ -28,6 +57,6 @@ mod tests {
 
     #[test]
     fn part1_input() {
-        assert_eq!(0, part1("input.txt"));
+        assert_eq!(13019, part1("input.txt"));
     }
 }
