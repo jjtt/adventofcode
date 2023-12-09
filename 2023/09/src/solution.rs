8c810391a3f9f99207551fcd63d42f004d69929d
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-09 07:31:47 +0200

    Day09, part2

--- 2023/09/src/solution.rs
+++ 2023/09/src/solution.rs
@@ -1,5 +1,3 @@
-use anyhow::bail;
-use scan_fmt::scan_fmt;
 use std::fs::read_to_string;
 
 fn next(seq: &[i32]) -> i32 {
@@ -9,6 +7,16 @@ fn next(seq: &[i32]) -> i32 {
     } else {
         let next_val = next(&next_seq);
         seq.last().expect("seq is not empty") + next_val
+    }
+}
+
+fn prev(seq: &[i32]) -> i32 {
+    let next_seq = seq.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
+    if next_seq.iter().all(|n| *n == 0) {
+        seq[0]
+    } else {
+        let prev_val = prev(&next_seq);
+        seq.first().expect("seq is not empty") - prev_val
     }
 }
 
@@ -27,13 +35,34 @@ pub fn part1(input: &str) -> i32 {
 }
 
 pub fn part2(input: &str) -> i32 {
-    //todo!()
-    0
+    let input = read_to_string(input).unwrap();
+    input
+        .trim()
+        .lines()
+        .map(|l| {
+            l.split(' ')
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .map(|seq| prev(&seq))
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn one_sequence_prev() {
+        let seq = vec![10, 13, 16, 21, 30, 45];
+        assert_eq!(5, prev(&seq));
+    }
+
+    #[test]
+    fn another_sequence_prev() {
+        let seq = vec![0, 2, 4, 6];
+        assert_eq!(-2, prev(&seq));
+    }
 
     #[test]
     fn one_sequence() {
@@ -42,7 +71,7 @@ mod tests {
     }
 
     #[test]
-    fn onother_sequence() {
+    fn another_sequence() {
         let seq = vec![0, 3, 6, 9, 12, 15];
         assert_eq!(18, next(&seq));
     }
@@ -54,6 +83,16 @@ mod tests {
 
     #[test]
     fn part1_input() {
-        assert_eq!(0, part1("input.txt"));
+        assert_eq!(1953784198, part1("input.txt"));
+    }
+
+    #[test]
+    fn part2_sample() {
+        assert_eq!(2, part2("sample.txt"));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(957, part2("input.txt"));
     }
 }
