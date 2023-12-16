1ab799291ddd159516c6d72ae00d6dc7f67b89d3
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-16 20:47:09 +0200

    Day14, part1

--- 2023/14/src/solution.rs
+++ 2023/14/src/solution.rs
@@ -1,10 +1,52 @@
 use anyhow::bail;
 use scan_fmt::scan_fmt;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
+enum Rock {
+    Round,
+    Cube,
+}
+
 pub fn part1(input: &str) -> usize {
-    //todo!()
-    0
+    let input = read_to_string(input).unwrap();
+    let mut height = 0;
+    let mut width = 0;
+    let rocks = input
+        .trim()
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            height += 1;
+            width = line.len();
+            line.chars().enumerate().filter_map(move |(x, c)| match c {
+                '.' => None,
+                '#' => Some(((x, y), Rock::Cube)),
+                'O' => Some(((x, y), Rock::Round)),
+                _ => panic!("invalid input"),
+            })
+        })
+        .collect::<HashMap<(usize, usize), Rock>>();
+
+    let mut total_load = 0;
+    let mut columns = vec![0; width];
+    for y in 0..height {
+        for x in 0..width {
+            let rock = rocks.get(&(x, y));
+            match rock {
+                Some(Rock::Cube) => {
+                    columns[x] = y + 1;
+                }
+                Some(Rock::Round) => {
+                    total_load += height - columns[x];
+                    columns[x] += 1;
+                }
+                None => {}
+            }
+        }
+    }
+
+    total_load
 }
 
 pub fn part2(input: &str) -> usize {
@@ -23,6 +65,6 @@ mod tests {
 
     #[test]
     fn part1_input() {
-        assert_eq!(0, part1("input.txt"));
+        assert_eq!(110821, part1("input.txt"));
     }
 }
