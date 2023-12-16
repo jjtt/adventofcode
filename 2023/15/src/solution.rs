4be7f7f55dde5272682464d7f09aa1a43a796822
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-16 21:32:57 +0200

    Day15, part2

--- 2023/15/src/solution.rs
+++ 2023/15/src/solution.rs
@@ -1,5 +1,6 @@
-use anyhow::bail;
-use scan_fmt::scan_fmt;
+
+use linked_hash_map::LinkedHashMap;
+
 use std::fs::read_to_string;
 
 pub fn part1(input: &str) -> usize {
@@ -8,8 +9,37 @@ pub fn part1(input: &str) -> usize {
 }
 
 pub fn part2(input: &str) -> usize {
-    //todo!()
-    0
+    let input = read_to_string(input).unwrap();
+
+    let mut boxes = vec![LinkedHashMap::<&str, usize>::new(); 256];
+    input.trim().split(',').for_each(|s| {
+        if s.ends_with('-') {
+            let label = &s[..s.len() - 1];
+            let box_id = hash(label);
+            boxes[box_id].remove(label);
+        } else {
+            let (label, lens) = s.split_once('=').expect("an assignment");
+            let lens = lens.parse::<usize>().expect("a number");
+            let box_id = hash(label);
+            let lenses = &mut boxes[box_id];
+            if lenses.contains_key(label) {
+                lenses.entry(label).and_modify(|l| *l = lens);
+            } else {
+                lenses.insert(label, lens);
+            }
+        }
+    });
+
+    boxes
+        .into_iter()
+        .enumerate()
+        .flat_map(|(box_index, lenses)| {
+            lenses
+                .into_iter()
+                .enumerate()
+                .map(move |(lens_index, (_label, lens))| (box_index + 1) * (lens_index + 1) * lens)
+        })
+        .sum()
 }
 
 fn hash(input: &str) -> usize {
@@ -30,6 +60,9 @@ mod tests {
     fn hashing() {
         assert_eq!(52, hash("HASH"));
         assert_eq!(30, hash("rn=1"));
+        assert_eq!(0, hash("rn"));
+        assert_eq!(1, hash("qp"));
+        assert_eq!(3, hash("pc"));
     }
 
     #[test]
@@ -40,5 +73,15 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(515974, part1("input.txt"));
+    }
+
+    #[test]
+    fn part2_sample() {
+        assert_eq!(145, part2("sample.txt"));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(265894, part2("input.txt"));
     }
 }
