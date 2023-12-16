cbf00ebf151584bf471f47a1f880fbc58f86c86e
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-16 21:00:54 +0200

    Day15 init

---
+++ 2023/15/src/solution.rs
@@ -0,0 +1,44 @@
+use anyhow::bail;
+use scan_fmt::scan_fmt;
+use std::fs::read_to_string;
+
+pub fn part1(input: &str) -> usize {
+    //todo!()
+    0
+}
+
+pub fn part2(input: &str) -> usize {
+    //todo!()
+    0
+}
+
+fn hash(input: &str) -> usize {
+    let mut current_value = 0;
+    input.chars().for_each(|c| {
+        current_value += c as usize;
+        current_value *= 17;
+        current_value %= 256;
+    });
+
+    current_value
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hashing() {
+        assert_eq!(52, hash("HASH"));
+    }
+
+    #[test]
+    fn part1_sample() {
+        assert_eq!(0, part1("sample.txt"));
+    }
+
+    #[test]
+    fn part1_input() {
+        assert_eq!(0, part1("input.txt"));
+    }
+}
