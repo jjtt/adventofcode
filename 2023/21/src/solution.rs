a4d6c73bb14980762522461ce69ca873d4a187a4
jjtt <3908945+jjtt@users.noreply.github.com>
2023-12-23 19:16:29 +0200

    Day21, part1

--- 2023/21/src/solution.rs
+++ 2023/21/src/solution.rs
@@ -1,10 +1,44 @@
-use anyhow::bail;
-use scan_fmt::scan_fmt;
+use std::collections::HashSet;
 use std::fs::read_to_string;
 
-pub fn part1_do(input: &str, steps: usize) -> usize {
-    //todo!()
-    0
+pub fn part1_do(input: &str, mut steps: usize) -> usize {
+    let input = read_to_string(input).unwrap();
+    let mut positions = HashSet::new();
+    let plots = input
+        .trim()
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            let mut plots_from_row = HashSet::new();
+            for (x, c) in line.chars().enumerate() {
+                plots_from_row.insert(match c {
+                    '.' => (x + 1, y + 1),
+                    'S' => {
+                        positions.insert((x + 1, y + 1));
+                        (x + 1, y + 1)
+                    }
+                    '#' => continue,
+                    _ => panic!("invalid char: {}", c),
+                });
+            }
+            plots_from_row
+        })
+        .collect::<HashSet<_>>();
+
+    while steps > 0 {
+        let mut new_positions = HashSet::new();
+        for &(x, y) in &positions {
+            for &(x, y) in &[(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)] {
+                if plots.contains(&(x, y)) {
+                    new_positions.insert((x, y));
+                }
+            }
+        }
+        positions = new_positions;
+        steps -= 1;
+    }
+
+    positions.len()
 }
 
 pub fn part1(input: &str) -> usize {
@@ -27,6 +61,6 @@ mod tests {
 
     #[test]
     fn part1_input() {
-        assert_eq!(0, part1("input.txt"));
+        assert_eq!(3646, part1("input.txt"));
     }
 }
