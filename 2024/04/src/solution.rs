5a7140dde121119c77f3ea7db658e47bf0b1d7b6
jjtt <3908945+jjtt@users.noreply.github.com>
2024-12-04 07:45:32 +0200

    Day04, part1

--- 2024/04/src/solution.rs
+++ 2024/04/src/solution.rs
@@ -1,10 +1,82 @@
 use anyhow::bail;
 use scan_fmt::scan_fmt;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Direction {
+    N,
+    NE,
+    E,
+    SE,
+    S,
+    SW,
+    W,
+    NW,
+}
+
 pub fn part1(input: &str) -> usize {
-    //todo!()
-    0
+    let map = parse(input);
+
+    map.iter()
+        .filter(|(_, &c)| c == 'X')
+        .map(|(pos, _)| count_xmases(&map, *pos))
+        .sum()
+}
+
+fn parse(input: &str) -> HashMap<(usize, usize), char> {
+    let input = read_to_string(input).unwrap();
+    let map: HashMap<_, _> = input
+        .trim()
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, c)| ((x + 1, y + 1), c))
+        })
+        .collect();
+    map
+}
+
+fn count_xmases(map: &HashMap<(usize, usize), char>, pos: (usize, usize)) -> usize {
+    let mut count = 0;
+    for &dir in &[
+        Direction::N,
+        Direction::NE,
+        Direction::E,
+        Direction::SE,
+        Direction::S,
+        Direction::SW,
+        Direction::W,
+        Direction::NW,
+    ] {
+        let mut pos = pos;
+        for l in "MAS".chars() {
+            pos = match dir {
+                Direction::N => (pos.0, pos.1 - 1),
+                Direction::NE => (pos.0 + 1, pos.1 - 1),
+                Direction::E => (pos.0 + 1, pos.1),
+                Direction::SE => (pos.0 + 1, pos.1 + 1),
+                Direction::S => (pos.0, pos.1 + 1),
+                Direction::SW => (pos.0 - 1, pos.1 + 1),
+                Direction::W => (pos.0 - 1, pos.1),
+                Direction::NW => (pos.0 - 1, pos.1 - 1),
+            };
+            if pos.0 == 0 || pos.1 == 0 {
+                break;
+            }
+            match map.get(&pos) {
+                Some(&c) if c == l => {
+                    if l == 'S' {
+                        count += 1
+                    }
+                }
+                _ => break,
+            }
+        }
+    }
+    count
 }
 
 pub fn part2(input: &str) -> usize {
@@ -23,6 +95,12 @@ mod tests {
 
     #[test]
     fn part1_input() {
-        assert_eq!(0, part1("input.txt"));
+        assert_eq!(2496, part1("input.txt"));
+    }
+
+    #[test]
+    fn counting_xmases() {
+        let map = parse("sample.txt");
+        assert_eq!(1, count_xmases(&map, (5, 1)));
     }
 }
