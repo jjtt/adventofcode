20accd81e407da9ffd898f450b00f7678aac87d9
jjtt <3908945+jjtt@users.noreply.github.com>
2024-12-07 00:47:23 +0200

    Day05, part1

--- 2024/05/src/solution.rs
+++ 2024/05/src/solution.rs
@@ -1,10 +1,55 @@
 use anyhow::bail;
 use scan_fmt::scan_fmt;
+use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 
 pub fn part1(input: &str) -> usize {
-    //todo!()
-    0
+    let input = read_to_string(input).unwrap();
+    let mut lines = input.lines();
+    let mut rules = true;
+    let mut lt = HashSet::new();
+    let mut gt = HashSet::new();
+    let mut pages = Vec::new();
+    for line in lines {
+        match line {
+            "" => {
+                rules = false;
+            }
+            _ if rules => {
+                let (left, right) = scan_fmt!(line, "{d}|{d}", usize, usize).unwrap();
+                assert!(lt.insert((left, right)));
+                assert!(gt.insert((right, left)));
+            }
+            _ => {
+                pages.push(
+                    line.split(",")
+                        .map(|n| n.parse::<usize>().unwrap())
+                        .collect::<Vec<_>>(),
+                );
+            }
+        }
+    }
+
+    let mut sum = 0;
+    for p in pages {
+        let mut sorted = p.clone();
+        sorted.sort_by(|a, b| {
+            if lt.contains(&(*a, *b)) {
+                return std::cmp::Ordering::Less;
+            } else if gt.contains(&(*a, *b)) {
+                return std::cmp::Ordering::Greater;
+            } else {
+                panic!("no rule for {} and {}", a, b);
+            }
+        });
+        if sorted == p {
+            // find middle item in p
+            let middle = p.len() / 2;
+            sum += p[middle];
+        }
+    }
+
+    sum
 }
 
 pub fn part2(input: &str) -> usize {
