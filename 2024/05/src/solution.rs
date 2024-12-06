e92df0b63579a5040599a11357d55080b1dfa8cc
jjtt <3908945+jjtt@users.noreply.github.com>
2024-12-07 00:52:00 +0200

    Day05, part2

--- 2024/05/src/solution.rs
+++ 2024/05/src/solution.rs
@@ -4,6 +4,37 @@ use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 
 pub fn part1(input: &str) -> usize {
+    let (lt, gt, pages) = parse(input);
+
+    let mut sum = 0;
+    for p in pages {
+        let mut sorted = p.clone();
+        sorted.sort_by(|a, b| {
+            if lt.contains(&(*a, *b)) {
+                return std::cmp::Ordering::Less;
+            } else if gt.contains(&(*a, *b)) {
+                return std::cmp::Ordering::Greater;
+            } else {
+                panic!("no rule for {} and {}", a, b);
+            }
+        });
+        if sorted == p {
+            // find middle item in p
+            let middle = p.len() / 2;
+            sum += p[middle];
+        }
+    }
+
+    sum
+}
+
+fn parse(
+    input: &str,
+) -> (
+    HashSet<(usize, usize)>,
+    HashSet<(usize, usize)>,
+    Vec<Vec<usize>>,
+) {
     let input = read_to_string(input).unwrap();
     let mut lines = input.lines();
     let mut rules = true;
@@ -29,6 +60,11 @@ pub fn part1(input: &str) -> usize {
             }
         }
     }
+    (lt, gt, pages)
+}
+
+pub fn part2(input: &str) -> usize {
+    let (lt, gt, pages) = parse(input);
 
     let mut sum = 0;
     for p in pages {
@@ -42,19 +78,13 @@ pub fn part1(input: &str) -> usize {
                 panic!("no rule for {} and {}", a, b);
             }
         });
-        if sorted == p {
-            // find middle item in p
-            let middle = p.len() / 2;
-            sum += p[middle];
+        if sorted != p {
+            let middle = sorted.len() / 2;
+            sum += sorted[middle];
         }
     }
 
     sum
-}
-
-pub fn part2(input: &str) -> usize {
-    //todo!()
-    0
 }
 
 #[cfg(test)]
@@ -68,6 +98,16 @@ mod tests {
 
     #[test]
     fn part1_input() {
-        assert_eq!(0, part1("input.txt"));
+        assert_eq!(4905, part1("input.txt"));
+    }
+
+    #[test]
+    fn part2_sample() {
+        assert_eq!(123, part2("sample.txt"));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(6204, part2("input.txt"));
     }
 }
