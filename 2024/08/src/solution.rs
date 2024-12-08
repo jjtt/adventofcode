435b5d717f9bba649eed7a290011d920ed4319f8
jjtt <3908945+jjtt@users.noreply.github.com>
2024-12-08 10:58:48 +0200

    Day08, part1

--- 2024/08/src/solution.rs
+++ 2024/08/src/solution.rs
@@ -1,10 +1,46 @@
 use anyhow::bail;
+use itertools::Itertools;
 use scan_fmt::scan_fmt;
+use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 
+fn parse(input: String) -> (i32, i32, HashMap<char, Vec<(i32, i32)>>) {
+    let cols = input.lines().next().unwrap().len();
+    let rows = input.lines().count();
+    let antennas = input
+        .trim()
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().filter_map(move |(x, c)| {
+                if c != '.' {
+                    Some((c, (x as i32, y as i32)))
+                } else {
+                    None
+                }
+            })
+        })
+        .into_group_map();
+    (cols as i32, rows as i32, antennas)
+}
+
 pub fn part1(input: &str) -> usize {
-    //todo!()
-    0
+    let input = read_to_string(input).unwrap();
+    let (cols, rows, antennas) = parse(input);
+
+    antennas
+        .values()
+        .flat_map(|antennas| {
+            antennas.iter().combinations(2).flat_map(|pair| {
+                assert_eq!(2, pair.len());
+                let (p1, p2) = (pair[0], pair[1]);
+                let (a1, a2) = antinodes(*p1, *p2);
+                [a1, a2]
+            })
+        })
+        .filter(|p| p.0 >= 0 && p.0 < cols && p.1 >= 0 && p.1 < rows)
+        .collect::<HashSet<_>>()
+        .len()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -12,9 +48,29 @@ pub fn part2(input: &str) -> usize {
     0
 }
 
+fn antinodes(p0: (i32, i32), p1: (i32, i32)) -> ((i32, i32), (i32, i32)) {
+    let dx = p1.0 - p0.0;
+    let dy = p1.1 - p0.1;
+    ((p1.0 + dx, p1.1 + dy), (p0.0 - dx, p0.1 - dy))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
+
+    #[test_case((4,3), (5,5), (3,1) => true)]
+    #[test_case((4,3), (5,5), (6,7) => true)]
+    #[test_case((5,5), (4,3), (3,1) => true)]
+    #[test_case((5,5), (4,3), (6,7) => true)]
+    #[test_case((5,5), (4,3), (1,1) => false)]
+    fn test_is_antinode(p1: (i32, i32), p2: (i32, i32), test: (i32, i32)) -> bool {
+        let (a1, a2) = antinodes(p1, p2);
+
+        dbg!(p1, p2, a1, a2);
+
+        a1 == test || a2 == test
+    }
 
     #[test]
     fn part1_sample() {
@@ -23,6 +79,6 @@ mod tests {
 
     #[test]
     fn part1_input() {
-        assert_eq!(0, part1("input.txt"));
+        assert_eq!(336, part1("input.txt"));
     }
 }
