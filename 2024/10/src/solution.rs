679c87a5de28c1923e37c6bcb67609e8043d6916
jjtt <3908945+jjtt@users.noreply.github.com>
2024-12-10 08:37:54 +0200

    Day10, init

---
+++ 2024/10/src/solution.rs
@@ -0,0 +1,27 @@
+use scan_fmt::scan_fmt;
+use std::fs::read_to_string;
+
+pub fn part1(input: &str) -> usize {
+    //todo!()
+    0
+}
+
+pub fn part2(input: &str) -> usize {
+    //todo!()
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_sample() {
+        assert_eq!(36, part1("sample.txt"));
+    }
+
+    #[test]
+    fn part1_input() {
+        assert_eq!(0, part1("input.txt"));
+    }
+}
